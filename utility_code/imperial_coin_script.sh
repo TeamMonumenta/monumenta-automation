a12eaede394b0a725a05ab3ddc18773e6b8838c1
Byron Marohn <combustible@live.com>
2017-10-30 20:50:24 -0700

    Removed obsolete imperial_coin and reset_command_generator scripts
    Signed-off-by: Byron Marohn <combustible@live.com>

--- utility_code/imperial_coin_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $# -ne 1 ]]; then
-	echo "Requires file argument"
-	exit 1
-fi
-
-perl -p -i -e 's|(^give [^ ]* *double_plant [0-9]) .*mperial.*oin.*$|\1 0 {display:{Name:"§6§lImperial Coin",Lore:["* Special Currency *"]},ench:[{id:51,lvl:1}]}|g' "$1"
-
