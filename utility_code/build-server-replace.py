af28e10451768eaddfc64c0a5c9060e32d6893a6
Byron Marohn <combustible@live.com>
2017-12-08 20:10:39 -0800

    Updated build-server-replace.py with new item replace config
    Signed-off-by: Byron Marohn <combustible@live.com>

--- utility_code/build-server-replace.py
+++ utility_code/build-server-replace.py
@@ -5,6 +5,8 @@ This takes a build world (terrain), a main world (play area), and
 merges them into a new world, dstWorld (destination).
 """
 
+import time
+
 from lib_monumenta.terrain_reset import terrainReset
 import item_replace_list
 
@@ -14,8 +16,8 @@ configList = [{
     # Dst is the destination world, which gets overwritten by the build world.
     # Then, data from the main world replaces the relevant parts of the dst world.
     # Please note that no special care need be taken with whitespace in filenames.
-    "localMainFolder":"/home/rock/project_epic/region_1/Project_Epic-region_1-prereplaceitems/",
-    "localDstFolder":"/home/rock/project_epic/region_1/Project_Epic-region_1/",
+    "localMainFolder":"/home/rock/tmp/Project_Epic-region_1-prereplaceitems/",
+    "localDstFolder":"/home/rock/tmp/Project_Epic-region_1/",
 
     # Which folder to copy the base world from. Either "build", "main", or not set
     "copyBaseFrom":"main",
@@ -28,10 +30,11 @@ configList = [{
     # List of places where item replacements should be run - options are "players", "world", "schematics"
     "itemReplacements":item_replace_list.itemReplacements,
     "itemReplaceLocations":["players", "world",],
+    "itemLog":"/home/rock/tmp/items_region_1.txt",
 }]
 
 print "If you didn't edit the replacement list to remove the decayed items, hit control+C now!"
+time.sleep(5)
 terrainReset(configList)
-item_replace_list.itemReplacements.PrintGlobalLog()
 print "Done"
 
