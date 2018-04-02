058e5c8bbbeebe1b6d6b57f53830c2af43d7878f
Byron Marohn <combustible@live.com>
2018-04-01 21:38:50 -0400

    Basic terrain reset bot framework function with timeouts
    Signed-off-by: Byron Marohn <combustible@live.com>

--- discord_bots/terrain_reset/build.py
+++ discord_bots/terrain_reset/build.py
@@ -8,9 +8,59 @@ logging.basicConfig(level=logging.INFO)
 import discord
 import asyncio
 import sys
+import os
+import subprocess
+import traceback
+import datetime
+
+################################################################################
+# Config / Environment
 
 client = discord.Client()
 channel = ""
+extraDebug = True
+lock = False
+
+################################################################################
+# Utility Functions
+
+def datestr():
+    return datetime.datetime.now().strftime("%Y_%m_%d")
+
+def split_string(text):
+    # Maximum number of characters in a single line
+    n = 1950
+
+    splits = text.splitlines()
+    result = []
+    cur = None
+    for i in splits:
+        while True:
+            if cur is None and len(i) <= n:
+                cur = i;
+                break # Done with i
+            elif cur is None and len(i) > n:
+                # Annoying case - one uber long line. Have to split into chunks
+                result = result + [i[:n]]
+                i = i[n:]
+                pass # Repeat this iteration
+            elif len(cur) + len(i) < n:
+                cur = cur + "\n" + i
+                break # Done with i
+            else:
+                result = result + [cur]
+                cur = None
+                pass # Repeat this iteration
+
+    if cur is not None:
+        result = result + [cur]
+        cur = None
+
+    return result
+
+
+################################################################################
+# Discord event handlers
 
 @client.event
 async def on_ready():
@@ -25,9 +75,11 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.startswith('!test'):
-        await client.send_message(message.channel, 'Got test')
-        await resetfunc()
-        sys.stdout.flush()
+        if lock:
+            await client.send_message(message.channel, 'Test already running, please wait')
+        else:
+            await client.send_message(message.channel, 'Running terrain reset')
+            await do_actions(terrain_reset)
 
     if message.content.startswith('!aoeu'):
         counter = 0
@@ -41,44 +93,130 @@ async def on_message(message):
                 await asyncio.sleep(5)
                 await client.send_message(message.channel, 'Done sleeping')
 
+################################################################################
+# Scriptable Routines
+
 async def sleep(seconds):
-    await client.send_message(channel, "Sleeping for " + str(seconds) + " seconds")
+    await display("Sleeping for " + str(seconds) + " seconds")
+    await asyncio.sleep(seconds)
 
 async def cd(path):
-    await client.send_message(channel, "Changing path to " + path)
+    if extraDebug:
+        await display("Changing path to `" + path + "`")
+    os.chdir(path)
 
-async def run(cmd, debuginfo=None, ret=0):
-    if debuginfo is not None:
-        await client.send_message(channel, debuginfo)
-    if cmd is not None:
-        await client.send_message(channel, "Executing: `" + cmd + "`")
+async def run(cmd, ret=0):
+    splitCmd = cmd.split(' ')
+    if extraDebug:
+        await display("Executing: ```" + str(splitCmd) + "```")
+    process = subprocess.run(splitCmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    rc = process.returncode
+
+    if extraDebug:
+        stdout = process.stdout.decode('utf-8')
+        await display("Result: {}".format(rc))
+        if stdout:
+            await display("stdout from command '{}':".format(cmd))
+            await display_verbatim(stdout)
+
+    stderr = process.stderr.decode('utf-8')
+    if stderr:
+        await display("stderr from command '{}':".format(cmd))
+        await display_verbatim(stderr)
+        # TODO: Remove likely
+        #if ret != None and ret == 0:
+            #raise ValueError("Got unexpected stderr while processing '{}'".format(cmd))
+
+    if ret != None and rc != ret:
+        raise ValueError("Expected result {}, got result {} while processing '{}'".format(ret, rc, cmd))
+
+async def assert_exists():
+    pass
+
+async def assert_equals():
+    pass
+
+async def display_verbatim(text):
+    for chunk in split_string(text):
+        await display("```" + chunk + "```")
 
 async def display(debuginfo):
-    run(None, debuginfo)
+    await client.send_message(channel, debuginfo)
 
-terrain_reset = [
-    run("mark2 send -n dungeon '~stop'", "Stopping the dungeon shard...", None),
-    sleep(10),
-    run("mark2 list | grep dungeon", "Checking that the dungeon shard is down...", 1),
-    run("cp -a ~/project_epic/dungeon/Project_Epic-dungeon ~/tmp/Project_Epic-dungeon", "Copying the dungeon data..."),
-    cd("~/project_epic/dungeon"),
-    run("mark2 start", "Restarting the dungeon shard..."),
+async def do_actions(actions):
+    try:
+        for item in actions:
+            await item
+    except Exception as e:
+        #TODO: Delete this line
+        #await client.send_message(channel, "**ERROR**: ```" + str(e) + "``` ```" + traceback.format_exc() + "```")
+        await client.send_message(channel, "**ERROR**: ```" + str(e) + "```")
 
-    cd("~/tmp"),
-    run("tar xzf ~/dungeon_template-keep-this-12-29-2017.tgz", "Unpacking the dungeon template..."),
-    run("~/MCEdit-And-Automation/utility_code/dungeon_instance_gen.py", "Running the dungeon instance generator..."),
-    run("mv ~/tmp/dungeons-out ~/tmp/POST_RESET"),
-    run("rm -rf ~/tmp/Project_Epic-dungeon ~/tmp/Project_Epic-template", "Cleaning up instance generation temp files..."),
+################################################################################
+# Terrain reset logic
+
+generate_instances = [
+    display("Cleaning up old terrain reset data..."),
+    run("rm -rf /home/rock/tmpreset", None),
+    run("mkdir -p /home/rock/tmpreset"),
+
+    display("Stopping the dungeon shard..."),
+    run("mark2 send -n dungeon ~stop", None),
+    sleep(5),
+    run("mark2 send -n dungeon test", 1),
+
+    display("Copying the dungeon master copies..."),
+    run("cp -a /home/rock/project_epic/dungeon/Project_Epic-dungeon /home/rock/tmpreset/Project_Epic-dungeon"),
+
+    display("Restarting the dungeon shard..."),
+    cd("/home/rock/project_epic/dungeon"),
+    run("mark2 start"),
+
+    display("Unpacking the dungeon template..."),
+    cd("/home/rock/tmpreset"),
+    run("tar xzf /home/rock/dungeon_template-keep-this-12-29-2017.tgz"),
+
+    display("Generating dungeon instances (this may take a while)..."),
+    run("python2 /home/rock/MCEdit-And-Automation/utility_code/dungeon_instance_gen.py"),
+    run("mv /home/rock/tmpreset/dungeons-out /home/rock/tmpreset/POST_RESET"),
+
+    display("Cleaning up instance generation temp files..."),
+    run("rm -rf /home/rock/tmpreset/Project_Epic-dungeon /home/rock/tmpreset/Project_Epic-template"),
     display("Dungeon instance generation complete!"),
 ]
 
+prep_reset_bundle = [
+    run("Stopping the region_1 shard..."),
+    run("mark2 send -n region_1 ~stop", None),
+    sleep(5),
+    run("mark2 send -n region_1 test", 1),
 
+    display("Copying region_1..."),
+    run("mkdir -p /home/rock/tmpreset/TEMPLATE/region_1"),
+    run("cp -a /home/rock/project_epic/region_1/Project_Epic-region_1 /home/rock/tmpreset/TEMPLATE/region_1/"),
 
+    display("Copying bungee..."),
+    run("cp -a /home/rock/project_epic/bungee /home/rock/tmpreset/TEMPLATE/"),
 
-async def resetfunc():
-    for item in terrain_reset:
-        status = await item
+    display("Copying purgatory..."),
+    run("cp -a /home/rock/project_epic/purgatory /home/rock/tmpreset/POST_RESET/"),
 
+    display("Copying tutorial..."),
+    run("mkdir /home/rock/tmpreset/POST_RESET/tutorial"),
+    run("cd /home/rock/tmpreset/POST_RESET/tutorial"),
+    run("tar xzf /home/rock/Project_Epic-tutorial.good.jan-12-2018.tgz"),
 
+    display("Copying server_config..."),
+    run("cp -a /home/rock/project_epic/server_config /home/rock/tmpreset/POST_RESET/"),
+
+    display("Packaging up reset bundle..."),
+    cd("/home/rock/tmpreset"),
+    run("tar czf /home/rock/tmpreset/project_epic_build_template_pre_reset_" + datestr() + ".tgz POST_RESET TEMPLATE"),
+]
+
+terrain_reset = generate_instances + prep_reset_bundle
+
+################################################################################
+# Entry point
 client.run('NDIwMDQ0ODMxMjg1NDQ0NjI5.DX49qQ.eIkPQMOllPAGKaaN8VInr6RKqHI')
 
