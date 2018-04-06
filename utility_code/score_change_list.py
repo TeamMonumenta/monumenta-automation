99f28f750a7063162547729b3f991dac6ef1388b
Tim Southwick <NickNackGus@gmail.com>
2018-04-06 19:50:49 -0400

    Forgot to make dungeon instances scores not persist for the instance preserving test

--- utility_code/score_change_list.py
+++ utility_code/score_change_list.py
@@ -9,79 +9,79 @@
 # appearing in the results, and runs actions for every name in the list.
 
 dungeonScoreRules = [
+#    {
+#        "condition":{"Objective":"D1Access","Score":{"min":1}},
+#        "actions":{"add":[
+#            {"Objective":"D1Access","Score":1000},
+#        ]},
+#    },
     {
         "condition":{"Objective":"D1Access","Score":{"min":1}},
-        "actions":{"add":[
-            {"Objective":"D1Access","Score":1000},
-        ]},
-    },
-    {
-        "condition":{"Objective":"D1Access","Score":{"min":2000}},
         "actions":{"set":[
             {"Objective":"D1Access","Score":0},
             {"Objective":"D1Finished","Score":0},
         ]},
     },
+#    {
+#        "condition":{"Objective":"D2Access","Score":{"min":1}},
+#        "actions":{"add":[
+#            {"Objective":"D2Access","Score":1000},
+#        ]},
+#    },
     {
         "condition":{"Objective":"D2Access","Score":{"min":1}},
-        "actions":{"add":[
-            {"Objective":"D2Access","Score":1000},
-        ]},
-    },
-    {
-        "condition":{"Objective":"D2Access","Score":{"min":2000}},
         "actions":{"set":[
             {"Objective":"D2Access","Score":0},
             {"Objective":"D2Finished","Score":0},
         ]},
     },
+#    {
+#        "condition":{"Objective":"D3Access","Score":{"min":1}},
+#        "actions":{"add":[
+#            {"Objective":"D3Access","Score":1000},
+#        ]},
+#    },
     {
         "condition":{"Objective":"D3Access","Score":{"min":1}},
-        "actions":{"add":[
-            {"Objective":"D3Access","Score":1000},
-        ]},
-    },
-    {
-        "condition":{"Objective":"D3Access","Score":{"min":2000}},
         "actions":{"set":[
             {"Objective":"D3Access","Score":0},
             {"Objective":"D3Finished","Score":0},
         ]},
     },
+#    {
+#        "condition":{"Objective":"D4Access","Score":{"min":1}},
+#        "actions":{"add":[
+#            {"Objective":"D4Access","Score":1000},
+#        ]},
+#    },
     {
         "condition":{"Objective":"D4Access","Score":{"min":1}},
-        "actions":{"add":[
-            {"Objective":"D4Access","Score":1000},
-        ]},
-    },
-    {
-        "condition":{"Objective":"D4Access","Score":{"min":2000}},
         "actions":{"set":[
             {"Objective":"D4Access","Score":0},
             {"Objective":"D4Finished","Score":0},
         ]},
     },
+#    {
+#        "condition":{"Objective":"D5Access","Score":{"min":1}},
+#        "actions":{"add":[
+#            {"Objective":"D5Access","Score":1000},
+#        ]},
+#    },
     {
         "condition":{"Objective":"D5Access","Score":{"min":1}},
-        "actions":{"add":[
-            {"Objective":"D5Access","Score":1000},
-        ]},
-    },
-    {
-        "condition":{"Objective":"D5Access","Score":{"min":2000}},
         "actions":{"set":[
             {"Objective":"D5Access","Score":0},
             {"Objective":"D5Finished","Score":0},
         ]},
     },
+#    {
+#        "condition":{"Objective":"DB1Access","Score":{"min":1}},
+#        "actions":{"add":[
+#            {"Objective":"DB1Access","Score":1000},
+#        ]},
+#    },
     {
         "condition":{"Objective":"DB1Access","Score":{"min":1}},
-        "actions":{"add":[
-            {"Objective":"DB1Access","Score":1000},
-        ]},
-    },
-    {
-        "condition":{"Objective":"DB1Access","Score":{"min":2000}},
         "actions":{"set":[
             {"Objective":"DB1Access","Score":0},
             {"Objective":"DB1Finished","Score":0},
