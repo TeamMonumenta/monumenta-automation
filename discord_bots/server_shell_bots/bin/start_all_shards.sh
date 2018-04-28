22dcd26b2bef3feb3e179e558fb253ce8742bf08
Byron Marohn <combustible@live.com>
2018-04-28 00:19:29 -0400

    Add short sleep to start_all_shards
    Signed-off-by: Byron Marohn <combustible@live.com>

--- discord_bots/server_shell_bots/bin/start_all_shards.sh
+++ discord_bots/server_shell_bots/bin/start_all_shards.sh
@@ -10,6 +10,7 @@ launch_server() (
 for x in betaplots build lightblue magenta nightmare orange purgatory r1bonus r1plots region_1 roguelike tutorial white yellow
 do
 	launch_server $x &
+	sleep 1
 done
 wait
 print 'All shards should be started. Please verify.'
