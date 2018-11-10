156c95dacf17ef12f7351e3ba9c80736b0a9ff0b
Monumenta <monumenta.mgmt@gmail.com>
2018-11-10 13:00:59 -0500

    Fix minor bugs in setup_datapacks
    Signed-off-by: Monumenta <monumenta.mgmt@gmail.com>

--- utility_code/setup_datapacks.py
+++ utility_code/setup_datapacks.py
@@ -7,11 +7,11 @@ from lib_py3.world import World
 # When to enable the dungeon data pack
 dungeons = ['white','orange','magenta','light_blue','yellow','r1bonus','roguelike','nightmare',]
 
-for shard in os.listdir('/home/rock/project_epic')
+for shard in os.listdir('/home/rock/project_epic'):
     if (
         shard == 'server_config' or
-        not os.is_dir( os.path.join( '/home/rock/project_epic', shard ) ) or
-        not os.is_file( os.path.join( '/home/rock/project_epic', shard, 'Project_Epic-' + shard, 'level.dat' ) )
+        not os.path.isdir( os.path.join( '/home/rock/project_epic', shard ) ) or
+        not os.path.isfile( os.path.join( '/home/rock/project_epic', shard, 'Project_Epic-' + shard, 'level.dat' ) )
     ):
         continue
 
