bdca89045d9a2f0d5b70b50f29b98fb39e27da82
Byron Marohn <combustible@live.com>
2018-12-08 00:40:07 -0500

    Overhaul raffle - hardcoded seed for now
    Signed-off-by: Byron Marohn <combustible@live.com>

--- utility_code/lib_py3/raffle.py
+++ utility_code/lib_py3/raffle.py
@@ -4,7 +4,8 @@ Raffle library - first used for weekly voting raffle
 """
 
 from math import ceil
-from numpy import random
+from collections import OrderedDict
+import pprint
 
 def normalized( a_list ):
     """
@@ -16,53 +17,126 @@ def normalized( a_list ):
         new_list.append( a_val / total )
     return new_list
 
-def vote_raffle( scoreboard, log_path, num_winners ):
+def vote_raffle( seed, scoreboard, log_path, num_winners ):
     logfp = open( log_path, "w" )
 
-    raffle_cache = scoreboard.get_cache( Objective=[ "VotesWeekly", "VoteRaffle" ] )
+    raffle_cache = scoreboard.get_cache( Objective=[ "VotesWeekly", "VotesSinceWin", "VoteRaffle" ] )
     weekly_scores = scoreboard.search_scores( Objective="VotesWeekly", Score={ "min": 1 }, Cache=raffle_cache )
+    since_win_scores = scoreboard.search_scores( Objective="VotesSinceWin", Score={ "min": 1 }, Cache=raffle_cache )
 
-    vote_names = []
-    vote_counts = []
-    vote_names_by_counts = {}
-    total_votes = 0
+    # Get the votes from this week
+    # Format: { "voter": (since_win, this_week) }
+    votes = OrderedDict()
+    total_votes_weekly = 0
     for a_score in weekly_scores:
         voter = a_score.at_path( "Name" ).value
-        num_votes = a_score.at_path( "Score" ).value
+        votes_weekly = a_score.at_path( "Score" ).value
 
-        vote_names.append( voter )
-        vote_counts.append( num_votes )
+        votes[voter] = (votes_weekly, votes_weekly)
 
-        if num_votes not in vote_names_by_counts.keys():
-            vote_names_by_counts[ num_votes ] = []
-        vote_names_by_counts[ num_votes ].append( voter )
+        total_votes_weekly += votes_weekly
 
-        total_votes += num_votes
+    # Get the votes since win
+    total_votes_since_win = total_votes_weekly
+    for a_score in since_win_scores:
+        voter = a_score.at_path( "Name" ).value
+        votes_since_win = a_score.at_path( "Score" ).value
 
-    logfp.write( "Total votes this week: {}\n".format( total_votes ) )
+        if voter in votes:
+            # This player also voted this week - add those
+            votes[voter] = (votes[voter][0] + votes_since_win, votes[voter][1])
 
-    if total_votes == 0:
+        else:
+            # This player did not vote this week but did since winning
+            votes[voter] = (votes_since_win, 0)
+
+        total_votes_since_win += votes_since_win
+
+    # Sort this week's votes
+    votes = OrderedDict(sorted(votes.items(), key=lambda kv: kv[1], reverse=True))
+
+    logfp.write( " Votes Since Win | Votes This Week | Name\n" )
+    logfp.write( "-----------------------------------------------------\n" )
+    for voter in votes:
+        logfp.write( " {} | {} | {}\n".format( str( votes[voter][0] ).rjust( 15 ), str( votes[voter][1] ).rjust( 15 ), voter ) )
+    logfp.write( "-----------------------------------------------------\n" )
+    logfp.write( " {} | {} | Total\n\n".format( str( total_votes_since_win ).rjust( 15 ), str( total_votes_weekly ).rjust( 15 )) )
+
+    if total_votes_since_win == 0:
         logfp.close
         return
 
-    for num_votes in sorted( vote_names_by_counts.keys(), reverse=True ):
-        vote_s = "s"
-        if num_votes == 1:
-            vote_s = ""
-        logfp.write( "{} vote{}:{}\n\n".format(
-            num_votes,
-            vote_s,
-            ", ".join( sorted( vote_names_by_counts[num_votes] ) )
-        ) )
+    # Reduce votes down to just the current list of votes
+    simple_votes = OrderedDict()
+    for voter in votes:
+        simple_votes[voter] = votes[voter][0]
+    votes = simple_votes
+    logfp.write('''
+Run this code with python 3 (requires python3-numpy) to verify the results of the raffle:
+################################################################################
 
-    winners = list( random.choice( vote_names, replace=False, size=num_winners, p=normalized( vote_counts ) ) )
-    winner_s = "s"
-    if len( winners ) == 1:
-        winner_s = ""
+from numpy import random
+import hashlib
+from collections import OrderedDict
+
+seed = \'\'\'{}\'\'\'
+num_winners = {}
+votes = {}
+
+# Split up into lists, count number of votes
+vote_names = []
+vote_scores = []
+total_votes = 0
+for voter in votes:
+    vote_names.append(voter)
+    vote_scores.append(votes[voter])
+    total_votes += votes[voter]
+
+# Convert string seed into a number, set the random number generator to start with that
+random.seed(int(hashlib.sha1(seed.encode('utf-8')).hexdigest()[:8], 16))
+
+# Pick winners
+winners = list(random.choice(vote_names, replace=False, size=num_winners, p=[vote/total_votes for vote in vote_scores]))
+print("This week's winners: " + ", ".join(sorted(winners)))
+
+################################################################################
+
+'''.format(seed, num_winners, pprint.pformat(votes)))
+
+    # Run exactly the same code in the printed snippet (print -> logfp.write)
+    #####################################################################################################
+    #
+    from numpy import random
+    import hashlib
+
+    #### Variables already defined
+
+    # Split up into lists, count number of votes
+    vote_names = []
+    vote_scores = []
+    total_votes = 0
+    for voter in votes:
+        vote_names.append(voter)
+        vote_scores.append(votes[voter])
+        total_votes += votes[voter]
+
+    # Convert string seed into a number, set the random number generator to start with that
+    random.seed(int(hashlib.sha1(seed.encode('utf-8')).hexdigest()[:8], 16))
+
+    # Pick winners
+    winners = list(random.choice(vote_names, replace=False, size=num_winners, p=[vote/total_votes for vote in vote_scores]))
+    logfp.write("This week's winners: " + ", ".join(sorted(winners)))
+
+    #
+    #####################################################################################################
+
+    # Set the winner's raffle scores, set their votes since win to 0
     for winner in winners:
         scoreboard.add_score( winner, "VoteRaffle", 1, Cache=raffle_cache )
+        votes[winner] = 0
+    for voter in votes:
+        scoreboard.set_score( voter, "VotesSinceWin", votes[voter], Cache=raffle_cache )
+
     scoreboard.save()
-    logfp.write( "This week's winner{}: ".format( winner_s ) + ", ".join( sorted( winners ) ) )
 
     logfp.close()
-
