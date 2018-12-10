ed422cabd33a2aca74c98de5fe745224cba81b93
Byron Marohn <combustible@live.com>
2018-12-09 20:23:51 -0500

    Upgrade make_item_loottable to 1.13
    Signed-off-by: Byron Marohn <combustible@live.com>

--- utility_code/make_item_loottable.py
+++ utility_code/make_item_loottable.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 
 import re
+import json
 
 while True:
     raw = input("Paste /give command here:")
-    split = raw.split()
 
-    if len(split) < 6:
-        print("Too few split components")
-        continue
+    # Strip the /give <selector>
+    raw = re.sub('^[^ ]* [^ ]* ', '', raw.strip())
 
-    item_type = split[2]
-    damage = split[4]
-    nbt = raw[raw.index(split[5]):]
+    # Strip the count
+    raw = re.sub('}[^}]*$', '}', raw.strip())
+
+    item_type = re.sub('{.*$', '', raw)
+    nbt = re.sub('^[^{]*{', '{', raw)
+
+    escaped_nbt = json.dumps(nbt, ensure_ascii=False)
 
     if "minecraft:" not in item_type:
         print("item type does not contain 'minecraft:'")
@@ -32,38 +35,39 @@ while True:
         continue
 
     namefrag = display[display.index('Name:'):]
-    name = re.findall(r'"(.*?)"', namefrag)[0]
+    # Name:"{\"text\":\"
+    name = re.sub(r'Name:"{\\"text\\":\\"', '', namefrag)
+    name = re.sub(r'\\.*$', '', name)
     name = name.lower()
     name = re.sub(" +", "_", name)
 
+    #print("item_type:", item_type)
+    #print("nbt:", nbt)
+    #print("escaped_nbt:", escaped_nbt)
+    #print("display:", display)
+    #print("namefrag:", namefrag)
+    #print("name:", name)
+
     outfile = open('./{}.json'.format(name), 'w')
     outfile.write('''{
-  "pools": [
-    {
-      "rolls": 1,
-      "entries": [
+    "pools": [
         {
-          "type": "item",
-          "name": "''' + item_type + '''",
-          "weight": 1,
-          "functions": [
-''')
-    if int(damage) != 0:
-        outfile.write('''            {
-              "function": "set_data",
-              "data": ''' + damage + '''
-            },
-''')
-
-    outfile.write('''            {
-              "function": "set_nbt",
-              "tag": "''' + nbt + '''"
-            }
-          ]
+            "rolls": 1,
+            "entries": [
+                {
+                    "type": "item",
+                    "weight": 1,
+                    "name": "''' + item_type + '''",
+                    "functions": [
+                        {
+                            "function": "set_nbt",
+                            "tag": ''' + escaped_nbt + '''
+                        }
+                    ]
+                }
+            ]
         }
-      ]
-    }
-  ]
+    ]
 }
 ''')
     outfile.close()
