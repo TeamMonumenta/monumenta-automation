1487227a29619a1c9ed579be9d39dca103c84125
Byron Marohn <combustible@live.com>
2019-01-06 17:10:32 -0500

    Recursive entity/tile entity iterator working
    Signed-off-by: Byron Marohn <combustible@live.com>

--- utility_code/lib_py3/world.py
+++ utility_code/lib_py3/world.py
@@ -277,6 +277,115 @@ class BaseChunkEntityIterator(object):
             return entity, False
 
 
+class RecursiveEntityIterator(object):
+    """
+    This iterator uses BaseChunkEntityIterator to get entities and tile entities in the world
+    Then it recursively iterates over them to find additional entities / tile entities
+
+    Same arguments as BaseChunkEntityIterator:
+
+    If readonly=False, it will save each chunk it visits that contain entities
+
+    If coordinates are specified, it will only load region files that contain those coordinates
+    Otherwise it will iterate over everything world wide
+
+    Only iterates over chunks in regions that could plausibly contain the specified coordinates
+    """
+
+    def __init__(self, world, pos1=None, pos2=None, readonly=True):
+        self._baseiterator = BaseChunkEntityIterator(world, pos1, pos2, readonly)
+
+    def __iter__(self):
+        """
+        Initialize the iterator for use.
+
+        Doing this here instead of in __init__ allows the iterator to potentially be re-used
+        """
+
+        # Initialize the base iterator
+        self._baseiterator.__iter__()
+
+        # Use a stack to keep track of what items still need to be processed
+        self._work_stack = []
+
+        return self
+
+    def _scan_item_for_work(self, item):
+        """
+        Looks at an item and if it contains more tile or block entities,
+        add them to the _work_stack
+        """
+        if item.has_path("tag.BlockEntityTag"):
+            self._work_stack.append((item.at_path("tag.BlockEntityTag"), True))
+
+        if item.has_path("tag.EntityTag"):
+            self._work_stack.append((item.at_path("tag.EntityTag"), False))
+
+    def __next__(self):
+        """
+        Iterates over entities embedded in an entity.
+
+        Iteration order is depth-first, returning the higher-level object first then using
+        a depth-first iterator into nested sub elements
+
+        Return value is two things!
+            entity - the entity OR tile entity TagCompound
+            isTileEntity - True if tile entity, False if entity
+        """
+
+        if len(self._work_stack) == 0:
+            # No work left to do - get another entity
+            entity, is_tile_entity = self._baseiterator.__next__()
+            self._work_stack.append((entity, is_tile_entity))
+
+        # Process the next work element on the stack
+        current_entity, is_tile_entity = self._work_stack.pop()
+
+        # Add more work to the stack for next time
+        if is_tile_entity:
+            # Tile entities!
+            if current_entity.has_path("SpawnPotentials"):
+                for spawn in current_entity.at_path("SpawnPotentials").value:
+                    if spawn.has_path("Entity"):
+                        self._work_stack.append((spawn.at_path("Entity"), False))
+
+            if current_entity.has_path("SpawnData"):
+                self._work_stack.append((current_entity.at_path("SpawnData"), False))
+
+            if current_entity.has_path("Items"):
+                for item in current_entity.at_path("Items").value:
+                    self._scan_item_for_work(item)
+
+            if current_entity.has_path("RecordItem"):
+                self._scan_item_for_work(current_entity.at_path("RecordItem"))
+
+        else:
+            # Regular entities!
+            if current_entity.has_path("Passengers"):
+                for passenger in current_entity.at_path("Passengers").value:
+                    self._work_stack.append((passenger, False))
+
+            if current_entity.has_path("Item"):
+                self._scan_item_for_work(current_entity.at_path("Item"))
+
+            if current_entity.has_path("HandItems"):
+                for item in current_entity.at_path("HandItems").value:
+                    self._scan_item_for_work(item)
+
+            if current_entity.has_path("ArmorItems"):
+                for item in current_entity.at_path("ArmorItems").value:
+                    self._scan_item_for_work(item)
+
+            if current_entity.has_path("Offers.Recipes"):
+                for item in current_entity.at_path("Offers.Recipes").value:
+                    if item.has_path("buy"):
+                        self._scan_item_for_work(item.at_path("buy"))
+                    if item.has_path("buyB"):
+                        self._scan_item_for_work(item.at_path("buyB"))
+                    if item.has_path("sell"):
+                        self._scan_item_for_work(item.at_path("sell"))
+
+        return current_entity, is_tile_entity
 
 class World(object):
     """
@@ -375,7 +484,7 @@ class World(object):
                 print("This is a regular entity!")
             entity.tree()
         '''
-        return BaseChunkEntityIterator(self, pos1, pos2, readonly)
+        return RecursiveEntityIterator(self, pos1, pos2, readonly)
 
     def find_data_packs(self):
         self._enabled_data_packs = []
