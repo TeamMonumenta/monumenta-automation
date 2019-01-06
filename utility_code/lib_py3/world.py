087ff9b6af03478e467b71176fe730831a9fb151
Byron Marohn <combustible@live.com>
2019-01-06 03:49:52 -0500

    TileEntityIterator cleverness removed, works correctly now
    Signed-off-by: Byron Marohn <combustible@live.com>

--- utility_code/lib_py3/world.py
+++ utility_code/lib_py3/world.py
@@ -86,47 +86,17 @@ class TileEntityIterator(object):
         Initialize the iterator for use.
 
         Doing this here instead of in __init__ allows the iterator to potentially be re-used
-
-        Note on how variables are used:
-            _chunk, _region, and _tile_entities are the currently active objects being iterated over
-
-            All of the index variables (_rx, _cx, _tile_entities_pos, etc.) are the index of
-            the NEXT element in that set to grab. This is sorta weird (use the variables now,
-            but increment them right before you're done) but it dramatically simplified the
-            logic
         """
 
         self._region = None
-        self._rx = self._min_x//512
-        self._rz = self._min_z//512
+        self._chunk = None
+        self._tile_entities = None
 
-        # Set to True when the currently loaded region is the last possible region
-        self._lastregion = False
-
-        # Load the first region/chunk
-        self._next_region()
-        self._next_chunk()
+        # The _first_run flag is used so that when the first call to __next__ is made, instead
+        # of trying to get the next chunk/region it loads the initial ones instead
+        self._first_run = True
 
         return self
-
-    def _update_next_region_coords(self):
-        """
-        Updates _rx and _rz to point to the next region that should be loaded
-
-        If out of regions, end iteration
-        """
-
-        if self._lastregion:
-            # All done!
-            raise StopIteration
-
-        self._rx += 1
-        if self._rx > self._max_x//512:
-            self._rx = self._min_x//512
-            self._rz += 1
-
-        if (self._rx == self._max_x//512) and (self._rz >= self._max_z//512):
-            self._lastregion = True
 
     def _next_region(self):
         """
@@ -139,56 +109,44 @@ class TileEntityIterator(object):
                 # Save the current region - maybe nothing to do here?
                 self._region = None
 
+            if self._first_run:
+                # This was the first call to __next__ - need to load the initial region files
+                # and not iterate them just yet
+                self._first_run = False
+                self._rx = self._min_x//512
+                self._rz = self._min_z//512
+            else:
+                # Get the next region
+                self._rx += 1
+                if self._rx > self._max_x//512:
+                    self._rx = self._min_x//512
+                    self._rz += 1
+
+                if self._rz > self._max_z//512:
+                    # All done!
+                    raise StopIteration
+
             # Load the next indicated region (_rx/_rz)
             region_path = os.path.join(self._world.path, "region", "r.{}.{}.mca".format(self._rx, self._rz))
 
-            # Before updating the _rx/_rz coords, use the current ones to calculate the bounding range
+            if not os.path.isfile(region_path):
+                # This region file isn't present - no reason to keep a reference for saving
+                self._region = None
+
+                # Continue iterating over regions to find the next valid one
+                continue
+
+            # Calculate which chunks need to be searched - don't waste time on chunks out of range
             self._cx_range = World.bounded_range(self._min_x, self._max_x, self._rx, 512, 16)
             self._cx_idx = 0
             self._cz_range = World.bounded_range(self._min_z, self._max_z, self._rz, 512, 16)
             self._cz_idx = 0
-            self._chunk = None
-            if len(self._cx_range) == 1 and len(self._cz_range) == 1:
-                self._lastchunk = True
-            else:
-                self._lastchunk = False
-
-
-            # Set the location of the next region
-            self._update_next_region_coords()
-
-            if not os.path.isfile(region_path):
-                # This region file isn't present - no reason to keep a reference for saving
-                self._chunk = None
-
-                # Continue iterating over regions to find the next valid one
-                continue
 
             # If we get to here, region_path is the valid next region file to work with
             self._region = nbt.RegionFile(region_path)
 
             # Successfully found next region - stop iterating
             break
-
-    def _update_next_chunk_coords(self):
-        """
-        Updates _cx and _cz to point to the next chunk that should be loaded
-
-        If out of chunks, advance to the next region
-        """
-        if self._lastchunk:
-            # Last chunk in this region - load the next region
-            # This also resets the cx/cz stuff and loads the first chunk
-            self._next_region()
-            return
-
-        self._cx_idx += 1
-        if self._cx_idx >= len(self._cx_range):
-            self._cx_idx = 0
-            self._cz_idx += 1
-
-        if (self._cx_idx == len(self._cx_range) - 1) and (self._cz_idx >= len(self._cz_range) - 1):
-            self._lastchunk = True
 
     def _next_chunk(self):
         """
@@ -198,15 +156,29 @@ class TileEntityIterator(object):
         """
         while True:
             if not self._chunk is None and not self._readonly:
-                # Save the current chunk
+                # Save the previously edited chunk
                 self._region.save_chunk(self._chunk)
                 self._chunk = None
 
+            if self._first_run:
+                # This was the first call to __next__ - need to load the initial region files
+                # and not iterate the chunk index's just yet
+                self._next_region()
+            else:
+                # Get the next chunk
+                self._cx_idx += 1
+                if self._cx_idx >= len(self._cx_range):
+                    self._cx_idx = 0
+                    self._cz_idx += 1
+
+            # If the next chunk index is out of range, move to the next region
+            if self._cz_idx >= len(self._cz_range):
+                # When moving to the next region, can be sure that the ranges are set correctly
+                # and can use the index's to get a real chunk in range
+                self._next_region()
+
             # Load the next indicated chunk (_cx/_cz)
             self._chunk = self._region.load_chunk(self._cx_range[self._cx_idx], self._cz_range[self._cz_idx])
-
-            # Set the location of the next chunk
-            self._update_next_chunk_coords()
 
             if (
                 (self._chunk is None) or
@@ -235,30 +207,30 @@ class TileEntityIterator(object):
         This does the final check that tile entities loaded are in the bounding box
         """
         while True:
-            if self._tile_entities_pos < len(self._tile_entities):
-                tile_entity = self._tile_entities[self._tile_entities_pos]
-                tile_x = tile_entity.at_path('x').value
-                tile_y = tile_entity.at_path('y').value
-                tile_z = tile_entity.at_path('z').value
+            if self._first_run or self._tile_entities_pos >= len(self._tile_entities):
+                # Out of tile entities - move to the next chunk with some to process
+                self._next_chunk()
 
-                # Increment index so regardless of whether this is in range the next step
-                # will find the next tile entity
-                self._tile_entities_pos += 1
+            tile_entity = self._tile_entities[self._tile_entities_pos]
+            tile_x = tile_entity.at_path('x').value
+            tile_y = tile_entity.at_path('y').value
+            tile_z = tile_entity.at_path('z').value
 
-                if not (
-                    self._min_x <= tile_x and tile_x <= self._max_x and
-                    self._min_y <= tile_y and tile_y <= self._max_y and
-                    self._min_z <= tile_z and tile_z <= self._max_z
-                ):
-                    # This tile entity isn't in the bounding box
-                    # Continue iterating until we find one that is
-                    continue
+            # Increment index so regardless of whether this is in range the next step
+            # will find the next tile entity
+            self._tile_entities_pos += 1
 
-                # Found a valid tile entity in range
-                return tile_entity
+            if not (
+                self._min_x <= tile_x and tile_x <= self._max_x and
+                self._min_y <= tile_y and tile_y <= self._max_y and
+                self._min_z <= tile_z and tile_z <= self._max_z
+            ):
+                # This tile entity isn't in the bounding box
+                # Continue iterating until we find one that is
+                continue
 
-            # Move to the next chunk with tile entities and try again
-            self._next_chunk()
+            # Found a valid tile entity in range
+            return tile_entity
 
 class World(object):
     """
