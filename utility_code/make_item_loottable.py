6bd3df43af48f4bee69b87dec03a72c8b9f2ae6e
Byron Marohn <combustible@live.com>
2019-01-11 00:20:22 -0500

    Add some janky stuff to make_item_loottable for uncommon chests
    Signed-off-by: Byron Marohn <combustible@live.com>

--- utility_code/make_item_loottable.py
+++ utility_code/make_item_loottable.py
@@ -5,6 +5,7 @@ import sys
 import json
 import re
 from collections import OrderedDict
+from pprint import pprint
 
 from lib_py3.json_file import jsonFile
 from lib_py3.common import eprint
@@ -13,12 +14,9 @@ from lib_py3.common import remove_formatting
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../quarry"))
 from quarry.types import nbt
 
-def make_single_loot_table(loot_table_path, entries):
-    pool = OrderedDict()
-    pool["rolls"] = 1
-    pool["entries"] = entries
+def make_single_loot_table(loot_table_path, pools):
     table_dict = OrderedDict()
-    table_dict["pools"] = [pool]
+    table_dict["pools"] = pools
 
     loot_table_string = json.dumps(table_dict, ensure_ascii=False, sort_keys=False, indent=4, separators=(',', ': '))
 
@@ -29,7 +27,7 @@ def make_single_loot_table(loot_table_path, entries):
     with open(loot_table_path, "w", encoding="utf-8") as f:
         f.write(loot_table_string)
 
-def make_loot_table(loot_table_base_path, container_nbt_list, loot_table_name=None):
+def make_loot_table(loot_table_base_path, container_nbt_list, loot_table_name=None, multi_pool=False):
     entries = None
     for container_nbt in container_nbt_list:
         if "Items" not in container_nbt.value:
@@ -40,39 +38,73 @@ def make_loot_table(loot_table_base_path, container_nbt_list, loot_table_name=No
             if not entries or not loot_table_name:
                 entries = []
 
-            # Fix minecraft annoyingly escaping apostraphes in item name
-            item_nbt.at_path("tag.display.Name").value = re.sub(r"\\u0027", "'", item_nbt.at_path("tag.display.Name").value)
-
             entry_json = OrderedDict()
             entry_json["type"] = "item"
             entry_json["weight"] = 10
             entry_json["name"] = item_nbt.at_path("id").value
-            entry_json["functions"] = [{
-                "function": "set_nbt",
-                "tag": item_nbt.at_path("tag").to_mojangson()
-            }]
+            entry_json["functions"] = []
 
-            item_name = item_nbt.at_path("tag.display.Name").value
-            item_name = remove_formatting(item_name)
-            # If the item name is JSON, parse it down to just the name text
-            try:
-                name_json = json.loads(item_name)
-                if "text" in name_json:
-                    item_name = name_json["text"]
-            except:
-                raise ValueError("WARNING: Item '" + item_name + "isn't json!")
+            # If item has a data tag, include it
+            if item_nbt.has_path('tag'):
+                func = OrderedDict()
+                func["function"] = "set_nbt"
+                func["tag"] = item_nbt.at_path("tag").to_mojangson()
+                entry_json["functions"].append(func)
+
+            # If count > 1 include it
+            if item_nbt.at_path('Count').value > 1:
+                func = OrderedDict()
+                func["function"] = "set_count"
+                func["count"] = item_nbt.at_path("Count").value
+                entry_json["functions"].append(func)
+
+            # If functions is empty, don't bother with it
+            if len(entry_json["functions"]) == 0:
+                entry_json.pop("functions")
 
             entries.append(entry_json)
 
             if not loot_table_name:
+                # If the item has a display name, grab it
+                if not item_nbt.has_path("tag.display.Name"):
+                    ValueError("ERROR: Single item loot table specified but item has no display name")
+
+                item_name = item_nbt.at_path("tag.display.Name").value
+                # Fix minecraft annoyingly escaping apostraphes in item name
+                item_name = re.sub(r"\\u0027", "'", item_name)
+                item_name = remove_formatting(item_name)
+                # If the item name is JSON, parse it down to just the name text
+                try:
+                    name_json = json.loads(item_name)
+                    if "text" in name_json:
+                        item_name = name_json["text"]
+                except:
+                    raise ValueError("WARNING: Item '" + item_name + "isn't json!")
                 filename = item_name.lower()
                 filename = re.sub(" +", "_", filename)
                 filename = "".join([i if re.match('[a-z0-9-_]', i) else '' for i in filename])
                 filename = filename + ".json"
-                make_single_loot_table(os.path.join(loot_table_base_path, filename), entries)
+
+                pool = OrderedDict()
+                pool["rolls"] = 1
+                pool["entries"] = entries
+                make_single_loot_table(os.path.join(loot_table_base_path, filename), [pool,])
 
     if loot_table_name:
-        make_single_loot_table(os.path.join(loot_table_base_path, loot_table_name), entries)
+        if multi_pool:
+            pools = []
+            for entry in entries:
+                pool = OrderedDict()
+                pool["rolls"] = 1
+                pool["entries"] = [entry]
+                pools.append(pool)
+            make_single_loot_table(os.path.join(loot_table_base_path, loot_table_name), pools)
+
+        else:
+            pool = OrderedDict()
+            pool["rolls"] = 1
+            pool["entries"] = entries
+            make_single_loot_table(os.path.join(loot_table_base_path, loot_table_name), pool)
 
 def usage():
     sys.exit("Usage: " + sys.argv[0] + " [-v, --verbose] <text_file_with_setblock_chest.txt> <output_directory> [loot_table_file_name]")
