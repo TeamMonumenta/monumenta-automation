2ebe1715efba661472a4b22a0e15fc7a382759ac
Byron Marohn <combustible@live.com>
2019-01-25 22:45:37 -0500

    Raffle seed
    Signed-off-by: Byron Marohn <combustible@live.com>

--- utility_code/raffle_results.py
+++ utility_code/raffle_results.py
@@ -12,7 +12,7 @@ if (len(sys.argv) != 3):
 
 scoreboard = Scoreboard(sys.argv[1])
 raffle_results = tempfile.mktemp()
-meme_seed = ''':TOOHYPE:'''
+meme_seed = '''ascendedmeme.png'''
 vote_raffle(meme_seed, scoreboard, raffle_results, int(sys.argv[2]))
 
 rafflefp = open( raffle_results, "r" )
