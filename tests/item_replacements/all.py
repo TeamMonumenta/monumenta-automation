b278165dcf1daae10fa368e3b082d1f741bd8f68
Tim Southwick <NickNackGus@gmail.com>
2019-03-19 02:14:23 -0400

    Added many new unit tests

--- tests/item_replacements/all.py
+++ tests/item_replacements/all.py
@@ -4,9 +4,11 @@ import importlib
 import os
 import sys
 
-sys.path.append(os.path.dirname(__file__))
+this_folder = os.path.dirname(__file__)
 
-for _package in os.listdir(os.path.dirname(__file__)):
+sys.path.append(this_folder)
+
+for _package in sorted(os.listdir(this_folder)):
     if not os.path.isdir(_package):
         continue
     if not os.path.isfile(os.path.join(_package,'__init__.py')):
--- tests/item_replacements/replacement_substitutions/double_json.py
+++ tests/item_replacements/replacement_substitutions/double_json.py
@@ -7,11 +7,11 @@ sys.path.append(os.path.join(os.path.dirname(__file__),'..'))
 
 from lib.base_test import ReplacementTest
 
-test_los_combustible = ReplacementTest(
+test_item = ReplacementTest(
     test_name="Substitutions: Fixed json in json names",
     template_item=r'''{id:"minecraft:golden_sword",tag:{display:{Lore:["§7Radiant","§7Hope","§8King's Valley : §4Artifact","§l","§7When in main hand:","§7 1.6 Attack Speed","§7 3 Attack Damage"],Name:"{\"text\":\"§e§l§nLight of Salvation\"}"}}}''',
     item_under_test=r'''{id:"minecraft:golden_sword",tag:{display:{Lore:["§7Radiant","§7Hope","§7Festive","§8King's Valley : §4Artifact","Infused by Combustible","§l","§7When in main hand:","§7 1.6 Attack Speed","§7 3 Attack Damage","* Soulbound to Combustible *"],Name:"{\"text\":\"{\\\"text\\\":\\\"§e§l§nLight of Salvation\\\"}\"}"}}}''',
     expected_result_item=r'''{id:"minecraft:golden_sword",tag:{display:{Lore:["§7Radiant","§7Hope","§7Festive","§8King's Valley : §4Artifact","Decorated by Combustible","§l","§7When in main hand:","§7 1.6 Attack Speed","§7 3 Attack Damage","* Soulbound to Combustible *"],Name:"{\"text\":\"§e§l§nLight of Salvation\"}"}}}'''
 )
-test_los_combustible.test()
+test_item.test()
 
