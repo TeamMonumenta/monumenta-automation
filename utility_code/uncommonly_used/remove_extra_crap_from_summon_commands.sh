42e9e7430f080ba7016ef9466e3f0be662410aa5
Byron Marohn <combustible@live.com>
2019-04-15 02:01:28 -0400

    A more formal entity junk tag remover
    Signed-off-by: Byron Marohn <combustible@live.com>

--- utility_code/uncommonly_used/remove_extra_crap_from_summon_commands.sh
+++
@@ -1,9 +0,0 @@
-for x in HurtByTimestamp FallFlying PortalCooldown AbsorptionAmount FallDistance DeathTime WorldUUIDMost UUIDMost UUIDLeast Spigot.ticksLived Dimension OnGround Air Bukkit.updateLevel Leashed Spigot.ticksLived CanPickUpLoot WorldUUIDLeast HurtTime LeftHanded Fire
-do
-	find . -name '*.mcfunction' | xargs perl -p -i -e "s|$x:[^,\]}]*,?||g"
-done
-
-for x in Motion Rotation ArmorDropChances Pos HandDropChances
-do
-	find . -name '*.mcfunction' | xargs perl -p -i -e "s|$x:\[[^\]]*\],?||g"
-done
