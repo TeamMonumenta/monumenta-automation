4cb65b53cf311dbd8dbb9b8ef2d30b09dd730d6f
Tim Southwick <NickNackGus@gmail.com>
2019-04-27 19:45:09 -0400

    Reduced timing issue

--- utility_code/loot_table_test_mobs.sh
+++ utility_code/loot_table_test_mobs.sh
@@ -2,7 +2,9 @@
 
 test_loot() (
 	mark2 send -n mobs "data merge block -1018 33 -1577 {Command:\"/fill -1024 16 -1584 -1009 30 -1569 minecraft:chest{LootTable:\\\"$1\\\"} replace minecraft:brown_wool\"}"
+	sleep 0.1
 	mark2 send -n mobs "setblock -1017 33 -1578 minecraft:redstone_block"
+	sleep 0.1
 	mark2 send -n mobs "save-all"
 	sleep 0.1
 
