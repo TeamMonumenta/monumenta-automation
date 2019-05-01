a942b110ea2c0031f9b33fb52b5b92fc8ba36f73
Timothy Southwick <NickNackGus@gmail.com>
2019-05-01 11:23:15 -0400

    Updated items in older tests

--- tests/item_replacements/replacement_substitutions/subtitute_item.py
+++ tests/item_replacements/replacement_substitutions/subtitute_item.py
@@ -9,9 +9,9 @@ from lib.base_test import ReplacementTest
 
 test_item = ReplacementTest(
     test_name="Substitutions: Substitute the ID and name of items, ignoring other NBT",
-    template_item=r'''{id:"minecraft:compass",tag:{display:{Lore:["§fLeft click to display active quest","§fRight click to cycle quests and objectives","§fShift + Right click to view local respawn timers"],Name:"{\"text\":\"§6§lQuest Compass\"}"}}}''',
-    item_under_test=r'''{id:"minecraft:trident",tag:{display:{Lore:["Items with no lore get skipped"],Name:"{\"text\":\"Dinglehopper\"}"}}}''',
-    expected_result_item=r'''{id:"minecraft:compass",tag:{display:{Lore:["§fLeft click to display active quest","§fRight click to cycle quests and objectives","§fShift + Right click to view local respawn timers"],Name:"{\"text\":\"§6§lQuest Compass\"}"}}}'''
+    template_item=r'''{id:"minecraft:anvil",tag:{display:{Name:"{\"text\":\"§aRepair Anvil\"}"}}}''',
+    item_under_test=r'''{id:"minecraft:damaged_anvil",tag:{display:{Name:"{\"text\":\"§aNarsen Anvil\"}"}}}''',
+    expected_result_item=r'''{id:"minecraft:anvil",tag:{display:{Name:"{\"text\":\"§aRepair Anvil\"}"}}}'''
 )
 test_item.test()
 
