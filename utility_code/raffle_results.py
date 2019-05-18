eb0e88308a3284815c5f12fd449ee319d69119da
Byron Marohn <combustible@live.com>
2019-05-18 01:26:40 -0400

    Raffle seed
    Signed-off-by: Byron Marohn <combustible@live.com>

--- utility_code/raffle_results.py
+++ utility_code/raffle_results.py
@@ -12,7 +12,7 @@ if (len(sys.argv) != 3):
 
 scoreboard = Scoreboard(sys.argv[1])
 raffle_results = tempfile.mktemp()
-meme_seed = '''R2 open beta at last'''
+meme_seed = ''':intensifies:'''
 vote_raffle(meme_seed, scoreboard, raffle_results, int(sys.argv[2]))
 
 rafflefp = open( raffle_results, "r" )
