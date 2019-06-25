95420c725d3f3c91ce1e1a31bc8a0d211ea6ec3a
Timothy Southwick <NickNackGus@gmail.com>
2019-06-24 22:25:07 -0400

    Updated tests with escaped single quotes

--- tests/item_replacements/replacement_substitutions/subtitute_item.py
+++ tests/item_replacements/replacement_substitutions/subtitute_item.py
@@ -10,7 +10,7 @@ from lib.base_test import ReplacementTest
 test_item = ReplacementTest(
     test_name="Substitutions: Substitute the ID and name of items, ignoring other NBT",
     template_item=r'''{id: "minecraft:nether_star",tag:{display:{Lore:["§7Worth 64 Compressed Crystalline Shards"],Name:"{\"text\":\"§b§lHyper Crystalline Shard\"}"}}}''',
-    item_under_test=r'''{id: "minecraft:nether_star",Count:3b,tag:{display:{Lore:["§7Worth 8 Compressed Crystalline Shards"],Name:"{\"text\":\"§b§lCompressed Crystalline Shard\"}"}}}''',
+    item_under_test=r'''{id: "minecraft:nether_star",Count:3b,tag:{display:{Lore:["§7Worth 8 Compressed Crystalline Shards"],Name:"{\"text\":\"§b§lPurified Crystalline Shard\"}"}}}''',
     expected_result_item=r'''{id: "minecraft:nether_star",Count:3b,tag:{display:{Lore:["§7Worth 64 Compressed Crystalline Shards"],Name:"{\"text\":\"§b§lHyper Crystalline Shard\"}"}}}'''
 )
 test_item.test()
