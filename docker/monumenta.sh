91d18de583e091b82ca6765e355b6c53da90e2a0
Byron Marohn <combustible@live.com>
2019-07-26 22:02:06 +0000

    Use resource limits for heap size, mobs prototype
    Signed-off-by: Byron Marohn <combustible@live.com>

--- docker/monumenta.sh
+++ docker/monumenta.sh
@@ -9,7 +9,6 @@ check_var() {
 	fi
 }
 
-check_var JAVA_MEM
 check_var SERVER_DIR
 check_var JAVA_JAR
 
@@ -18,11 +17,23 @@ if [[ ! -d "$SERVER_DIR" ]]; then
 	exit 1
 fi
 
-args="-Xmx$JAVA_MEM -Xms$JAVA_MEM -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -XX:+AlwaysPreTouch -Dusing.aikars.flags=mcflags.emc.gs"
 
+args="-XX:+UnlockExperimentalVMOptions"
+
+# If memory isn't specified, use all of the amount set by the current cgroup
+if [[ "$JAVA_MEM " != " " ]]; then
+	args="$args -Xmx$JAVA_MEM -Xms$JAVA_MEM"
+else
+	args="$args -XX:+UseCGroupMemoryLimitForHeap"
+fi
+
+# If JAVA_LARGE_PAGES variable is not empty, use hugepages
 if [[ "$JAVA_LARGE_PAGES " != " " ]]; then
 	args="$args -XX:LargePageSizeInBytes=2M -XX:+UseLargePages -XX:+UseLargePagesInMetaspace"
 fi
+
+# Remaining standard arguments
+args="$args -XX:+UseG1GC -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -XX:+AlwaysPreTouch -Dusing.aikars.flags=mcflags.emc.gs"
 
 cd "$SERVER_DIR"
 echo "Executing:"
