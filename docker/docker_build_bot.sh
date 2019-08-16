fff03edc1540e0447dfda0809afec688ebeacb5f
Byron Marohn <combustible@live.com>
2019-08-16 18:48:03 -0400

    Convenience scripts for rebuilding the bot docker images
    Signed-off-by: Byron Marohn <combustible@live.com>

---
+++ docker/docker_build_bot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build .. --file automation-bot.Dockerfile -t monumenta-docker.injic.io/monumenta-automation-bot --build-arg USERNAME=epic --build-arg UID=1000 --build-arg GID=1000 && docker push monumenta-docker.injic.io/monumenta-automation-bot
