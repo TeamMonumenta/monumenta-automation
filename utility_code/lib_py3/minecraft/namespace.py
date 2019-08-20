3d319192d29fb44d2dabad5129b9dc507e92b6fb
Timothy Southwick <NickNackGus@gmail.com>
2019-08-20 18:17:19 -0400

    Namespace code is ready for use, may be imported where needed

--- utility_code/lib_py3/minecraft/namespace.py
+++ utility_code/lib_py3/minecraft/namespace.py
@@ -1,26 +1,38 @@
 #!/usr/bin/env python3
 
 import copy
+import os
 import re
 
 valid_namespace_re = re.compile(r"""^[-_0-9a-z]+$""")
 valid_namespace_path_re = re.compile(r"""^[-_0-9a-z/]+$""")
 
-class Namespace(object):
-    def __init__(self, other):
+class Namespace(dict):
+    def __init__(self, other, contents={}):
         if isinstance(other, type(self)):
             self.namespace = other.namespace
-            self.contents = copy.deepcopy(other.contents)
+            for key, value in other.items():
+                self[key] = value
+            for key, value in contents.items():
+                self[key] = value
 
         elif isinstance(other, str):
             if not valid_namespace_re.match(other):
-                
+                raise SyntaxError("namespace may only contain [-_0-9a-z]")
 
             self.namespace = other
-            self.contents = {}
+            for key, value in contents.items():
+                self[key] = value
 
         else:
             raise TypeError()
+
+    def __setitem__(self, key, value):
+        namespaced_id = NamespacedId(key)
+        super().__setitem__(namespaced_id, value)
+
+    def __repr__(self):
+        return "Namespace(other={!r}, contents={!r})".format(self.namespace, super().__repr__())
 
 class NamespacedId(object):
     def __init__(self, other):
@@ -31,6 +43,7 @@ class NamespacedId(object):
 
         elif isinstance(other, str):
             parts = other.split(":")
+            parts[-1] = parts[-1].replace(os.sep, '/')
             if len(parts) > 2:
                 raise ValueError("other may only contain one ':'.")
 
@@ -68,6 +81,16 @@ class NamespacedId(object):
 
     def to_pair(self):
         return (self.namespace, self.key)
+
+    def to_path(self, root='', middle=''):
+        """Get this namespaced ID as a path.
+
+        For example:
+        >>> test = NamespacedId('monumenta:wool')
+        >>> test.to_path('data', 'tags/blocks')
+        'data/monumenta/tags/blocks/wool'
+        """
+        return os.path.join(root, self.namespace, middle, self.key.replace('/', os.sep))
 
     def __str__(self):
         return self.namespace + ":" + self.key
