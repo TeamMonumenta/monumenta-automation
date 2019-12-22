65f8087d378394ba89959982e2b21ca785958476
Byron Marohn <combustible@live.com>
2019-12-22 13:34:33 -0500

    Update tp_to_spawn script
    Signed-off-by: Byron Marohn <combustible@live.com>

---
+++ utility_code/tp_to_spawn.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import sys
+
+from lib_py3.terrain_reset import terrain_reset_instance
+
+if (len(sys.argv) < 2):
+    sys.exit("Usage: {} <server1> [server2] ...".format(sys.argv[0]))
+
+valid_names = (
+    "region_1",
+    "region_2",
+    "white",
+    "orange",
+    "magenta",
+    "lightblue",
+    "yellow",
+    "lime",
+    "pink",
+    "gray",
+    "lightgray",
+    "cyan",
+    "willows",
+    "reverie",
+    "sanctum",
+    "labs",
+    "shiftingcity",
+    "roguelike",
+    "tutorial",
+)
+
+reset_name_list = []
+reset_config_list = []
+for arg in sys.argv[1:]:
+    if arg in valid_names:
+        reset_name_list.append(arg)
+        reset_config_list.append({
+            "server":arg,
+            "localDstFolder":"{0}/Project_Epic-{0}/".format(arg),
+            "tpToSpawn":True,
+        })
+    else:
+        print("ERROR: Unknown shard {} specified!".format(arg))
+        sys.exit("Usage: {} <server1> [server2] ...".format(sys.argv[0]))
+
+for config in reset_config_list:
+    terrain_reset_instance(config)
+
+print("Shards reset successfully: {}".format(reset_name_list))
