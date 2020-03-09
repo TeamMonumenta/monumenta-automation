ec61e268ffdac16cd1c4f570159c5d5346ede025
Byron Marohn <combustible@live.com>
2020-03-08 21:37:55 -0400

    Made rabbitmq 'sockets' library more resiliant
    Signed-off-by: Byron Marohn <combustible@live.com>

--- utility_code/lib_py3/lib_sockets.py
+++ utility_code/lib_py3/lib_sockets.py
@@ -6,6 +6,7 @@ import logging
 import threading
 import pika
 import traceback
+import time
 
 logger = logging.getLogger(__name__)
 
@@ -16,14 +17,8 @@ class SocketManager(object):
         self._callback = callback
         self._rabbit_host = rabbit_host
         self._durable_queue = durable
+        self._channel = None
         logger.setLevel(log_level);
-
-        # Create the connection and declare the queue
-        # pika is not thread safe - need a channel per thread
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host=self._rabbit_host))
-        self._channel = connection.channel()
-        logger.debug("Declaring queue {}".format(self._queue_name))
-        self._channel.queue_declare(queue=self._queue_name, durable=self._durable_queue)
 
         # Create a thread to connect to the queue and block / consume messages
         self.thread = threading.Thread(target=self._subscriber)
@@ -46,20 +41,24 @@ class SocketManager(object):
 
             channel.basic_ack(delivery_tag=method_frame.delivery_tag)
 
-        try:
-            # Create the connection and declare the queue
-            connection = pika.BlockingConnection(pika.ConnectionParameters(host=self._rabbit_host))
-            channel = connection.channel()
-            logger.debug("Declaring queue {}".format(self._queue_name))
-            channel.queue_declare(queue=self._queue_name, durable=self._durable_queue)
+        while True:
+            try:
+                # Create the connection and declare the queue
+                connection = pika.BlockingConnection(pika.ConnectionParameters(host=self._rabbit_host))
+                channel = connection.channel()
+                logger.debug("Declaring queue {}".format(self._queue_name))
+                channel.queue_declare(queue=self._queue_name, durable=self._durable_queue)
 
-            ## Start consuming messages
-            logger.info("Started rabbitmq message consumer for queue {}".format(self._queue_name))
-            channel.basic_consume(queue=self._queue_name, on_message_callback=callback)
-            channel.start_consuming()
-        except Exception as e:
-            logger.warn('Rabbitmq consumer thread failed: {}'.format(e))
-            logger.warn(traceback.format_exc())
+                ## Start consuming messages
+                logger.info("Started rabbitmq message consumer for queue {}".format(self._queue_name))
+                channel.basic_consume(queue=self._queue_name, on_message_callback=callback)
+                channel.start_consuming()
+            except Exception as e:
+                logger.warn('Rabbitmq consumer thread failed: {}'.format(e))
+                logger.warn(traceback.format_exc())
+
+            logger.warn('Attempting to reconnect rabbitmq consumer...')
+            time.sleep(10)
 
     def send_packet(self, destination, operation, data):
         if destination is None or len(destination) == 0:
@@ -68,6 +67,15 @@ class SocketManager(object):
             logger.warn("operation can not be None!")
         if data is None or len(data) == 0:
             logger.warn("data can not be None!")
+
+        if self._channel is None or self._channel.is_closed:
+            # Create the connection and declare the queue
+            # pika is not thread safe - need a channel per thread
+            connection = pika.BlockingConnection(pika.ConnectionParameters(host=self._rabbit_host))
+            self._channel = connection.channel()
+
+        if self._channel.is_closed:
+            raise Exception("Failed to send message to rabbitmq despite attempting to reconnect")
 
         packet = {}
         packet["source"] = self._queue_name
