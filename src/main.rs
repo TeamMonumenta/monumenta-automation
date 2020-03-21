dfbf5511c94cd76017e2b3dbbb6cef474ab1344d
Byron Marohn <combustible@live.com>
2020-03-21 22:45:15 +0000

    Working prototype for one datapack
    Signed-off-by: Byron Marohn <combustible@live.com>

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,15 @@ use std::collections::HashMap;
 use regex::Regex;
 
 
-#[derive(PartialEq, Eq, Hash)]
+#[derive(PartialEq, Eq, Hash, Clone)]
+enum NamespaceType {
+    Advancement,
+    Function
+}
+
+#[derive(PartialEq, Eq, Hash, Clone)]
 struct NamespacedKey {
+    key_type: NamespaceType,
     namespace: String,
     key: String,
 }
@@ -25,9 +32,9 @@ impl NamespacedKey {
                 let key = split[1];
 
                 if key.ends_with(".json") && key.starts_with("advancements") {
-                    return Some(NamespacedKey{namespace: namespace.to_string(), key: key.trim_start_matches("advancements/").trim_end_matches(".json").to_string()});
+                    return Some(NamespacedKey{key_type: NamespaceType::Advancement, namespace: namespace.to_string(), key: key.trim_start_matches("advancements/").trim_end_matches(".json").to_string()});
                 } else if key.ends_with(".mcfunction") && key.starts_with("functions") {
-                    return Some(NamespacedKey{namespace: namespace.to_string(), key: key.trim_start_matches("functions/").trim_end_matches(".mcfunction").to_string()});
+                    return Some(NamespacedKey{key_type: NamespaceType::Function, namespace: namespace.to_string(), key: key.trim_start_matches("functions/").trim_end_matches(".mcfunction").to_string()});
                 }
             }
         }
@@ -35,10 +42,10 @@ impl NamespacedKey {
         None
     }
 
-    fn from_str(path: &str) -> Option<NamespacedKey> {
+    fn from_str(path: &str, key_type: NamespaceType) -> Option<NamespacedKey> {
         let split: Vec<&str> = path.splitn(2, ":").collect();
         if split.len() == 2 {
-            return Some(NamespacedKey{namespace: split[0].to_string(), key: split[1].to_string()});
+            return Some(NamespacedKey{key_type: key_type, namespace: split[0].to_string(), key: split[1].to_string()});
         }
 
         None
@@ -47,13 +54,19 @@ impl NamespacedKey {
 
 impl fmt::Display for NamespacedKey {
     fn fmt(&self, f: &mut fmt::Formatter) -> std::result::Result<(), std::fmt::Error> {
-        write!(f, "{}:{}", self.namespace, self.key)
+        match self.key_type {
+            NamespaceType::Advancement => write!(f, "advancement({}:{})", self.namespace, self.key),
+            NamespaceType::Function => write!(f, "function({}:{})", self.namespace, self.key)
+        }
     }
 }
 
 impl fmt::Debug for NamespacedKey {
     fn fmt(&self, f: &mut fmt::Formatter) -> std::result::Result<(), std::fmt::Error> {
-        write!(f, "{}:{}", self.namespace, self.key)
+        match self.key_type {
+            NamespaceType::Advancement => write!(f, "advancement({}:{})", self.namespace, self.key),
+            NamespaceType::Function => write!(f, "function({}:{})", self.namespace, self.key)
+        }
     }
 }
 
@@ -61,7 +74,7 @@ impl fmt::Debug for NamespacedKey {
 struct Advancement {
     path: String,
     parent: Option<NamespacedKey>,
-    children: Vec<*mut NamespacedItem>,
+    children: Vec<NamespacedKey>,
     used: bool,
     data: serde_json::value::Value
 }
@@ -69,7 +82,7 @@ struct Advancement {
 #[derive(Debug)]
 struct Function {
     path: String,
-    children: Vec<*mut NamespacedItem>,
+    children: Vec<NamespacedKey>,
     used: bool,
     data: String
 }
@@ -80,10 +93,40 @@ enum NamespacedItem {
     Function(Function)
 }
 
+impl NamespacedItem {
+    fn get_children(&self) -> &Vec<NamespacedKey> {
+        match self {
+            NamespacedItem::Advancement(advancement) => &advancement.children,
+            NamespacedItem::Function(function) => &function.children,
+        }
+    }
+
+    fn get_path(&self) -> &str {
+        match self {
+            NamespacedItem::Advancement(advancement) => &advancement.path,
+            NamespacedItem::Function(function) => &function.path,
+        }
+    }
+
+    fn is_used(&self) -> bool {
+        match self {
+            NamespacedItem::Advancement(advancement) => advancement.used,
+            NamespacedItem::Function(function) => function.used,
+        }
+    }
+
+    fn set_used(&mut self, val: bool) {
+        match self {
+            NamespacedItem::Advancement(advancement) => advancement.used = val,
+            NamespacedItem::Function(function) => function.used = val,
+        }
+    }
+}
+
 fn get_function<'c>(advancement: &'c serde_json::Value) -> Option<NamespacedKey> {
     if let Some(serde_json::Value::Object(rewards)) = advancement.get("rewards") {
         if let Some(serde_json::Value::String(function)) = rewards.get("function") {
-            return NamespacedKey::from_str(function);
+            return NamespacedKey::from_str(function, NamespaceType::Function);
         }
     }
     None
@@ -94,10 +137,12 @@ fn load_file(path: String,
 
     if let Ok(file) = fs::read_to_string(&path) {
         if path.ends_with(".json") {
-            let json: serde_json::Value = serde_json::from_str(&file).expect(&format!("Failed to parse file as json: {}", file));
-
-            if let Some(namespace) = NamespacedKey::from_path(path.trim_start_matches(datapacks_path)) {
-                return Some((namespace, NamespacedItem::Advancement(Advancement{ path: path, parent: None, children: vec!(), used: false, data: json })))
+            if let Ok(json) = serde_json::from_str(&file) {
+                if let Some(namespace) = NamespacedKey::from_path(path.trim_start_matches(datapacks_path)) {
+                    return Some((namespace, NamespacedItem::Advancement(Advancement{ path: path, parent: None, children: vec!(), used: false, data: json })))
+                }
+            } else {
+                println!("Warning: Failed to parse file as json: {}", path);
             }
         } else if path.ends_with(".mcfunction") {
             if let Some(namespace) = NamespacedKey::from_path(path.trim_start_matches(datapacks_path)) {
@@ -105,7 +150,7 @@ fn load_file(path: String,
             }
         }
     } else {
-        println!("Warning: Failed to open file: {}", &path);
+        println!("Warning: Failed to read file as string: {}", &path);
     }
 
     None
@@ -114,81 +159,114 @@ fn load_file(path: String,
 fn main() {
     let dir = "/home/bmarohn/home/datapacks/region_1";
 
-    let mut advancements: HashMap<NamespacedKey, NamespacedItem> = HashMap::new();
-    let mut functions: HashMap<NamespacedKey, NamespacedItem> = HashMap::new();
+    let mut items: HashMap<NamespacedKey, NamespacedItem> = HashMap::new();
 
     /* Load files into the advancements/functions maps */
     for entry in WalkDir::new(dir).follow_links(true) {
         if let Ok(entry) = entry {
             if entry.path().is_file() {
                 if let Some((namespace, item)) = load_file(entry.path().to_str().unwrap().to_string(), dir) {
-                    match item {
-                        /* TODO: Handle overlaps from different datapacks ? */
-                        NamespacedItem::Advancement(_) => { advancements.insert(namespace, item); }
-                        NamespacedItem::Function(_) => { functions.insert(namespace, item); }
-                    };
+                    /* TODO: Handle overlaps from different datapacks ? */
+                    items.insert(namespace, item);
                 }
             }
+        }
+    }
+
+    let re_func = Regex::new(r"function [a-z][^ :]*:[a-z][^ :]*").unwrap();
+    let re_adv = Regex::new(r"advancement (grant|revoke) .* (everything|from|only|through|until) [a-z][^ :]*:[a-z][^ :]*").unwrap();
+    let re_adv_only = Regex::new(r" only [a-z][^ :]*:[a-z][^ :]*").unwrap();
+
+    /* A list of all the namespaced keys that are used but have not yet had their children processed */
+    let mut pending: Vec<NamespacedKey> = Vec::new();
+
+    /* Create links between the various files */
+    for (key, val) in items.iter_mut() {
+        match val {
+            NamespacedItem::Advancement(advancement) => {
+                /* Create reference links for advancements */
+
+                /* Link to parent advancement - mostly just interesting, not actually used */
+                if let Some(serde_json::Value::String(parent)) = advancement.data.get("parent") {
+                    advancement.parent = NamespacedKey::from_str(parent, NamespaceType::Advancement);
+                }
+
+                /* Link to function */
+                if let Some(run_func) = get_function(&advancement.data) {
+                    advancement.children.push(run_func);
+                }
+
+                /* Mark as used by default unless it has an impossible trigger */
+                advancement.used = true;
+                if let Some(serde_json::Value::Object(criteria)) = advancement.data.get("criteria") {
+                    for (_c_key, c_value) in criteria.iter() {
+                        if let Some(serde_json::Value::String(trigger)) = c_value.get("trigger") {
+                            if trigger == "minecraft:impossible" {
+                                advancement.used = false;
+                            }
+                        }
+                    }
+                }
+
+                if advancement.used {
+                    /* Advancement is used - push it and its children to the pending list */
+                    pending.push(key.clone());
+                    pending.extend(advancement.children.iter().cloned());
+                }
+            }
+            NamespacedItem::Function(function) => {
+                /* Create reference links for functions */
+                for line in function.data.lines() {
+
+                    /* Link functions to the other functions they call */
+                    /* Schedule function and function both have the same syntax, so one match will do */
+                    if let Some(func_match) = re_func.find(line) {
+                        if let Some(key) = NamespacedKey::from_str(func_match.as_str().trim_start_matches("function "), NamespaceType::Function) {
+                            function.children.push(key);
+                        }
+                    }
+
+                    /* Link functions to advancements */
+                    if let Some(adv_match) = re_adv.find(line) {
+                        if let Some(adv_only_match) = re_adv_only.find(adv_match.as_str()) {
+                            if let Some(key) = NamespacedKey::from_str(adv_only_match.as_str().trim_start_matches(" only "), NamespaceType::Advancement) {
+                                function.children.push(key);
+                            }
+                        } else {
+                            println!("Warning: No support for advancement command {}", adv_match.as_str());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /* Iterate through the pending list and mark things as used */
+    while pending.len() > 0 {
+        let key = pending.pop().unwrap();
+        if let Some(map_value) = items.get_mut(&key) {
+            /*
+             * Only do work on nodes not marked as used. If a node is already
+             * marked as used, it has either already had its children marked
+             * or it is in the pending list
+             */
+            if !map_value.is_used() {
+                map_value.set_used(true);
+                /* Copy the children into the pending list */
+                pending.extend(map_value.get_children().iter().cloned());
+            }
+        } else {
+            println!("Warning: Missing item {}", key);
         }
     }
 
     println!("\n");
+    println!("Used:");
 
-    /* Create reference links for advancements */
-    for (_key, val) in advancements.iter_mut() {
-        if let NamespacedItem::Advancement(advancement) = val {
-            /* Link to parent advancement - mostly just interesting, not actually used */
-            if let Some(serde_json::Value::String(parent)) = advancement.data.get("parent") {
-                advancement.parent = NamespacedKey::from_str(parent);
-            }
-
-            /* Link to function */
-            if let Some(run_func) = get_function(&advancement.data) {
-                if let Some(function) = functions.get_mut(&run_func) {
-                    let raw_function: *mut _ = &mut *function;
-                    advancement.children.push(raw_function);
-                }
-            }
-
-            /* Mark as used by default unless it has an impossible trigger */
-            advancement.used = true;
-            if let Some(serde_json::Value::Object(criteria)) = advancement.data.get("criteria") {
-                for (_c_key, c_value) in criteria.iter() {
-                    if let Some(serde_json::Value::String(trigger)) = c_value.get("trigger") {
-                        if trigger == "minecraft:impossible" {
-                            advancement.used = false;
-                        }
-                    }
-                }
-            }
+    for (_, val) in items.iter() {
+        if val.is_used() {
+            println!("  {}", val.get_path());
         }
     }
 
-    /* Create reference links for functions */
-    let re = Regex::new(r"function [a-z][^ :]*:[a-z][^ :]*").unwrap();
-    for (_, val) in functions.iter_mut() {
-        if let NamespacedItem::Function(function) = val {
-            for line in function.data.lines() {
-
-                /* Link functions to the other functions they call */
-                /* Schedule function and function both have the same syntax, so one match will do */
-                if let Some(func_match) = re.find(line) {
-                    if let Some(key) = NamespacedKey::from_str(func_match.as_str().trim_start_matches("function ")) {
-                        if let Some(called_function) = functions.get_mut(&key) {
-                            let raw_function: *mut _ = &mut *called_function;
-                            function.children.push(raw_function);
-                        }
-
-
-                        println!("{}", key)
-                    }
-                }
-
-                // "function tape_measure:feet_from"
-
-
-
-            }
-        }
-    }
 }
