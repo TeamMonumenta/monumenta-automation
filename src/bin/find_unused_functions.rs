8fdc9bfd9ac1cd741b975ee72ed424ad5d61b2d3
Byron Marohn <combustible@live.com>
2020-03-23 01:55:23 +0000

    Load quests files as first-class items
    Signed-off-by: Byron Marohn <combustible@live.com>

--- src/bin/find_unused_functions.rs
+++ src/bin/find_unused_functions.rs
@@ -1,3 +1,4 @@
+#[macro_use] extern crate simple_error;
 #[macro_use] extern crate log;
 #[macro_use] extern crate lazy_static;
 
@@ -15,7 +16,8 @@ type BoxResult<T> = Result<T,Box<dyn Error>>;
 #[derive(PartialEq, Eq, Hash, Clone)]
 enum NamespaceType {
     Advancement,
-    Function
+    Function,
+    Quest
 }
 
 #[derive(PartialEq, Eq, Hash, Clone)]
@@ -26,10 +28,11 @@ struct NamespacedKey {
 }
 
 impl NamespacedKey {
-    fn from_path(path: &str) -> Option<NamespacedKey> {
+    fn from_path(path: &str) -> BoxResult<NamespacedKey> {
         let path = path.trim_start_matches("/");
 
         if path.starts_with("data") {
+            /* Assume it is a datapack entry */
             let path = path.trim_start_matches("data").trim_start_matches("/");
 
             let split: Vec<&str> = path.splitn(2, "/").collect();
@@ -38,17 +41,24 @@ impl NamespacedKey {
                 let key = split[1];
 
                 if key.ends_with(".json") && key.starts_with("advancements") {
-                    return Some(NamespacedKey{key_type: NamespaceType::Advancement, namespace: namespace.to_string(), key: key.trim_start_matches("advancements/").trim_end_matches(".json").to_string()});
+                    return Ok(NamespacedKey{key_type: NamespaceType::Advancement, namespace: namespace.to_string(), key: key.trim_start_matches("advancements/").trim_end_matches(".json").to_string()});
                 } else if key.ends_with(".mcfunction") && key.starts_with("functions") {
-                    return Some(NamespacedKey{key_type: NamespaceType::Function, namespace: namespace.to_string(), key: key.trim_start_matches("functions/").trim_end_matches(".mcfunction").to_string()});
+                    return Ok(NamespacedKey{key_type: NamespaceType::Function, namespace: namespace.to_string(), key: key.trim_start_matches("functions/").trim_end_matches(".mcfunction").to_string()});
                 }
             }
-        } else {
-            error!("Error: Datapack does not contain 'data' subfolder!");
-            panic!()
-        }
+        } else if path.ends_with(".json") {
+            /* Assume it is a quest */
+            let split: Vec<&str> = path.splitn(2, "/").collect();
+            if split.len() == 2 {
+                let namespace = split[0];
+                let key = split[1];
 
-        None
+                if key.ends_with(".json") {
+                    return Ok(NamespacedKey{key_type: NamespaceType::Quest, namespace: namespace.to_string(), key: key.trim_end_matches(".json").to_string()});
+                }
+            }
+        }
+        bail!("Failed to convert path {} to NamespacedKey", path);
     }
 
     fn from_str(path: &str, key_type: NamespaceType) -> Option<NamespacedKey> {
@@ -65,7 +75,8 @@ impl fmt::Display for NamespacedKey {
     fn fmt(&self, f: &mut fmt::Formatter) -> std::result::Result<(), std::fmt::Error> {
         match self.key_type {
             NamespaceType::Advancement => write!(f, "advancement({}:{})", self.namespace, self.key),
-            NamespaceType::Function => write!(f, "function({}:{})", self.namespace, self.key)
+            NamespaceType::Function => write!(f, "function({}:{})", self.namespace, self.key),
+            NamespaceType::Quest => write!(f, "quest({}:{})", self.namespace, self.key),
         }
     }
 }
@@ -74,7 +85,8 @@ impl fmt::Debug for NamespacedKey {
     fn fmt(&self, f: &mut fmt::Formatter) -> std::result::Result<(), std::fmt::Error> {
         match self.key_type {
             NamespaceType::Advancement => write!(f, "advancement({}:{})", self.namespace, self.key),
-            NamespaceType::Function => write!(f, "function({}:{})", self.namespace, self.key)
+            NamespaceType::Function => write!(f, "function({}:{})", self.namespace, self.key),
+            NamespaceType::Quest => write!(f, "quest({}:{})", self.namespace, self.key),
         }
     }
 }
@@ -85,7 +97,7 @@ struct Advancement {
     children: Vec<NamespacedKey>,
     used: bool,
     data: Vec<String>,
-    json_data: Vec<serde_json::value::Value>
+    json_data: Vec<serde_json::value::Value>,
 }
 
 #[derive(Debug)]
@@ -93,13 +105,27 @@ struct Function {
     path: Vec<String>,
     children: Vec<NamespacedKey>,
     used: bool,
-    data: Vec<String>
+    data: Vec<String>,
+}
+
+#[derive(Debug)]
+struct Quest {
+    /*
+     * These will never have more than one entry - but keeping them as vectors
+     * makes them easier to operate on
+     */
+    path: Vec<String>,
+    children: Vec<NamespacedKey>, /* TODO: Currently always empty! */
+    used: bool, /* TODO: Currently always true! */
+    data: Vec<String>,
+    json_data: Vec<serde_json::value::Value>,
 }
 
 #[derive(Debug)]
 enum NamespacedItem {
     Advancement(Advancement),
-    Function(Function)
+    Function(Function),
+    Quest(Quest),
 }
 
 impl NamespacedItem {
@@ -107,6 +133,7 @@ impl NamespacedItem {
         match self {
             NamespacedItem::Advancement(advancement) => &advancement.children,
             NamespacedItem::Function(function) => &function.children,
+            NamespacedItem::Quest(quest) => &quest.children,
         }
     }
 
@@ -128,6 +155,9 @@ impl NamespacedItem {
                     panic!("Function and advancement with same namespace!");
                 }
             }
+            inner @ NamespacedItem::Quest(_) => {
+                panic!("Conflicting quests found! {:?} and {:?}", inner.get_paths(), variant.get_paths());
+            }
         }
     }
 
@@ -135,6 +165,7 @@ impl NamespacedItem {
         match self {
             NamespacedItem::Advancement(advancement) => &advancement.path,
             NamespacedItem::Function(function) => &function.path,
+            NamespacedItem::Quest(quest) => &quest.path,
         }
     }
 
@@ -142,6 +173,7 @@ impl NamespacedItem {
         match self {
             NamespacedItem::Advancement(advancement) => advancement.used,
             NamespacedItem::Function(function) => function.used,
+            NamespacedItem::Quest(quest) => quest.used,
         }
     }
 
@@ -149,6 +181,7 @@ impl NamespacedItem {
         match self {
             NamespacedItem::Advancement(advancement) => advancement.used = val,
             NamespacedItem::Function(function) => function.used = val,
+            NamespacedItem::Quest(quest) => quest.used = val,
         }
     }
 }
@@ -168,15 +201,22 @@ fn load_datapack_file(path: String, datapacks_path: &str) -> Option<(NamespacedK
         if let Ok(file) = fs::read_to_string(&path) {
             if path.ends_with(".json") {
                 if let Ok(json) = serde_json::from_str(&file) {
-                    if let Some(namespace) = NamespacedKey::from_path(path.trim_start_matches(datapacks_path)) {
+                    if let Ok(namespace) = NamespacedKey::from_path(path.trim_start_matches(datapacks_path)) {
                         return Some((namespace, NamespacedItem::Advancement(Advancement{ path: vec!(path), children: vec!(), used: false, data: vec!(file), json_data: vec!(json) })))
+                    } else {
+                        /* Suppress warnings about intentionally failing objects */
+                        if !path.contains("loot_tables") && !path.contains("recipes") && !path.contains("/tags/") {
+                            warn!("Failed to create namespaced key for: {}", path);
+                        }
                     }
                 } else {
                     warn!("Failed to parse file as json: {}", path);
                 }
             } else if path.ends_with(".mcfunction") {
-                if let Some(namespace) = NamespacedKey::from_path(path.trim_start_matches(datapacks_path)) {
+                if let Ok(namespace) = NamespacedKey::from_path(path.trim_start_matches(datapacks_path)) {
                     return Some((namespace, NamespacedItem::Function(Function{ path: vec!(path), children: vec!(), used: false, data: vec!(file) })))
+                } else {
+                    warn!("Failed to create namespaced key for: {}", path);
                 }
             }
         } else {
@@ -187,14 +227,18 @@ fn load_datapack_file(path: String, datapacks_path: &str) -> Option<(NamespacedK
     None
 }
 
-fn load_quests(pending: &mut Vec<NamespacedKey>, dir: &str) {
+fn load_quests(items: &mut HashMap<NamespacedKey, NamespacedItem>, dir: &str) {
     for entry in WalkDir::new(dir).follow_links(true) {
         if let Ok(entry) = entry {
             let path = entry.path().to_str().unwrap();
             if entry.path().is_file() && path.ends_with(".json") {
                 if let Ok(file) = fs::read_to_string(&path) {
                     if let Ok(json @ serde_json::value::Value::Object(_)) = serde_json::from_str(&file) {
-                        load_quest_file_recursive(pending, &json);
+                        if let Ok(namespace) = NamespacedKey::from_path(path.trim_start_matches(dir)) {
+                            items.insert(namespace, NamespacedItem::Quest(Quest{ path: vec!(path.to_string()), children: vec!(), used: false, data: vec!(file), json_data: vec!(json) }));
+                        } else {
+                            warn!("Failed to create namespaced key for: {}", path);
+                        }
                     } else {
                         warn!("Failed to parse file as json: {}", path);
                     }
@@ -234,11 +278,11 @@ fn load_datapack(items: &mut HashMap<NamespacedKey, NamespacedItem>,
     }
 }
 
-fn load_quest_file_recursive(pending: &mut Vec<NamespacedKey>, value: &serde_json::value::Value) {
+fn link_quest_file_recursive(pending: &mut Vec<NamespacedKey>, value: &serde_json::value::Value) {
     match value {
         serde_json::value::Value::Array(array) => {
             for item in array.iter() {
-                load_quest_file_recursive(pending, item);
+                link_quest_file_recursive(pending, item);
             }
         }
         serde_json::value::Value::Object(obj) => {
@@ -256,7 +300,7 @@ fn load_quest_file_recursive(pending: &mut Vec<NamespacedKey>, value: &serde_jso
                         }
                     }
                 } else {
-                    load_quest_file_recursive(pending, value);
+                    link_quest_file_recursive(pending, value);
                 }
             }
         }
@@ -375,6 +419,14 @@ fn create_links(items: &mut HashMap<NamespacedKey, NamespacedItem>,
                     }
                 }
             }
+            NamespacedItem::Quest(quest) => {
+                /* Create reference links for functions */
+                for data in quest.json_data.iter() {
+                    /* TODO: Quest is always used and has no children */
+                    quest.used = true;
+                    link_quest_file_recursive(pending, &data);
+                }
+            }
         }
     }
 }
@@ -409,7 +461,7 @@ fn main() -> BoxResult<()> {
         }
     }
 
-    load_quests(&mut pending, "/home/bmarohn/home/scriptedquests");
+    load_quests(&mut items, "/home/bmarohn/home/scriptedquests");
 
     create_links(&mut items, &mut pending);
 
