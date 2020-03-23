d5ecad2d02d63bbe2d89cb88ab5f8da839be293a
Byron Marohn <combustible@live.com>
2020-03-23 01:15:14 +0000

    Refactor getting scoreboard usage distribution into scoreboard lib
    Signed-off-by: Byron Marohn <combustible@live.com>

--- src/scoreboard.rs
+++ src/scoreboard.rs
@@ -54,6 +54,53 @@ impl ScoreboardCollection {
     pub fn objectives(&self) -> &HashSet<String> {
         &self.objectives
     }
+
+    pub fn get_objective_usage(&self) -> Vec<(String, f64)> {
+        /*
+         * Iterate over all scoreboards and accumulate the nonzero_count and total_entries for each
+         * objective
+         *
+         * key = objective name
+         * val = (nonzero_count, total_entries)
+         */
+        let mut counts: HashMap<String, (i32, i32)> = HashMap::new();
+
+        for (_, scoreboard) in self.scoreboards.iter() {
+            for (objective_name, objective) in scoreboard.objectives.iter() {
+                let mut nonzero_count = 0;
+                let mut total_entries = 0;
+
+                /* Retrieve stored values from the hash table if they are present */
+                if let Some((stored_nonzero_count, stored_total_entries)) = counts.get(objective_name) {
+                    nonzero_count = *stored_nonzero_count;
+                    total_entries = *stored_total_entries;
+                }
+
+                for (_, score) in objective.data.iter() {
+                    total_entries += 1;
+                    if score.score != 0 {
+                        nonzero_count += 1
+                    }
+                }
+
+                /*
+                 * This avoids 0/0 later. Just assume each objective had at least one item in it that
+                 * was 0
+                 */
+                if total_entries == 0 {
+                    total_entries = 1;
+                }
+
+                counts.insert(objective_name.to_string(), (nonzero_count, total_entries));
+            }
+        }
+
+        /* Create a vector from the hash table data and sort it by value */
+        let mut count_vec: Vec<(String, f64)> = counts.iter().map(|(objective_name, (nonzero_count, total_entries))| (objective_name.to_string(), *nonzero_count as f64 / *total_entries as f64)).collect();
+        count_vec.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+
+        count_vec
+    }
 }
 
 impl Scoreboard {
