e81f76ecea9de8fec3418959566bc1b7ea3cc45c
Byron Marohn <combustible@live.com>
2020-03-23 23:20:59 +0000

    Commands parsing now works like all other node types
    Signed-off-by: Byron Marohn <combustible@live.com>

--- src/bin/find_unused_components.rs
+++ src/bin/find_unused_components.rs
@@ -17,7 +17,8 @@ type BoxResult<T> = Result<T,Box<dyn Error>>;
 enum NamespaceType {
     Advancement,
     Function,
-    Quest
+    Quest,
+    Command,
 }
 
 #[derive(PartialEq, Eq, Hash, Clone)]
@@ -61,13 +62,16 @@ impl NamespacedKey {
         bail!("Failed to convert path {} to NamespacedKey", path);
     }
 
-    fn from_str(path: &str, key_type: NamespaceType) -> Option<NamespacedKey> {
+    fn from_str(path: &str, key_type: NamespaceType) -> BoxResult<NamespacedKey> {
         let split: Vec<&str> = path.splitn(2, ":").collect();
         if split.len() == 2 {
-            return Some(NamespacedKey{key_type: key_type, namespace: split[0].to_string(), key: split[1].to_string()});
+            return Ok(NamespacedKey{key_type: key_type, namespace: split[0].to_string(), key: split[1].to_string()});
         }
+        bail!("Failed to split string {} on ':'", path);
+    }
 
-        None
+    fn from_command(world: &str, coords: &str) -> NamespacedKey {
+        NamespacedKey{ key_type: NamespaceType::Command, namespace: world.to_string(), key: coords.to_string() }
     }
 }
 
@@ -77,6 +81,7 @@ impl fmt::Display for NamespacedKey {
             NamespaceType::Advancement => write!(f, "advancement({}:{})", self.namespace, self.key),
             NamespaceType::Function => write!(f, "function({}:{})", self.namespace, self.key),
             NamespaceType::Quest => write!(f, "quest({}:{})", self.namespace, self.key),
+            NamespaceType::Command => write!(f, "command({}:{})", self.namespace, self.key),
         }
     }
 }
@@ -87,6 +92,7 @@ impl fmt::Debug for NamespacedKey {
             NamespaceType::Advancement => write!(f, "advancement({}:{})", self.namespace, self.key),
             NamespaceType::Function => write!(f, "function({}:{})", self.namespace, self.key),
             NamespaceType::Quest => write!(f, "quest({}:{})", self.namespace, self.key),
+            NamespaceType::Command => write!(f, "command({}:{})", self.namespace, self.key),
         }
     }
 }
@@ -111,10 +117,17 @@ struct Function {
 #[derive(Debug)]
 struct Quest {
     path: Vec<String>,
-    children: Vec<NamespacedKey>, /* TODO: Currently always empty! */
+    children: Vec<NamespacedKey>,
     used: bool, /* TODO: Currently always true! */
     data: String,
     json_data: serde_json::value::Value,
+}
+
+#[derive(Debug)]
+struct Command {
+    path: Vec<String>,
+    children: Vec<NamespacedKey>,
+    data: Vec<String>, /* Commands can be at the same location if in a chest */
 }
 
 #[derive(Debug)]
@@ -122,6 +135,7 @@ enum NamespacedItem {
     Advancement(Advancement),
     Function(Function),
     Quest(Quest),
+    Command(Command),
 }
 
 impl NamespacedItem {
@@ -130,6 +144,7 @@ impl NamespacedItem {
             NamespacedItem::Advancement(advancement) => &advancement.children,
             NamespacedItem::Function(function) => &function.children,
             NamespacedItem::Quest(quest) => &quest.children,
+            NamespacedItem::Command(command) => &command.children,
         }
     }
 
@@ -140,7 +155,7 @@ impl NamespacedItem {
                     inner.path.extend(var_inner.path.iter().cloned());
                     inner.data.extend(var_inner.data.iter().cloned());
                 } else {
-                    panic!("Advancement and function with same namespace!");
+                    panic!("Advancement and other with same namespace!");
                 }
             }
             NamespacedItem::Function(inner) => {
@@ -148,11 +163,19 @@ impl NamespacedItem {
                     inner.path.extend(var_inner.path.iter().cloned());
                     inner.data.extend(var_inner.data.iter().cloned());
                 } else {
-                    panic!("Function and advancement with same namespace!");
+                    panic!("Function and other with same namespace!");
                 }
             }
             inner @ NamespacedItem::Quest(_) => {
                 panic!("Conflicting quests found! {:?} and {:?}", inner.get_paths(), variant.get_paths());
+            }
+            NamespacedItem::Command(inner) => {
+                if let NamespacedItem::Command(var_inner) = variant {
+                    inner.path.extend(var_inner.path.iter().cloned());
+                    inner.data.extend(var_inner.data.iter().cloned());
+                } else {
+                    panic!("Command and other with same namespace!");
+                }
             }
         }
     }
@@ -161,7 +184,8 @@ impl NamespacedItem {
         match self {
             NamespacedItem::Advancement(advancement) => &advancement.path,
             NamespacedItem::Function(function) => &function.path,
-            NamespacedItem::Quest(quest) => &quest.path
+            NamespacedItem::Quest(quest) => &quest.path,
+            NamespacedItem::Command(command) => &command.path,
         }
     }
 
@@ -170,6 +194,7 @@ impl NamespacedItem {
             NamespacedItem::Advancement(advancement) => advancement.used,
             NamespacedItem::Function(function) => function.used,
             NamespacedItem::Quest(quest) => quest.used,
+            NamespacedItem::Command(_) => true,
         }
     }
 
@@ -178,6 +203,7 @@ impl NamespacedItem {
             NamespacedItem::Advancement(advancement) => advancement.used = val,
             NamespacedItem::Function(function) => function.used = val,
             NamespacedItem::Quest(quest) => quest.used = val,
+            NamespacedItem::Command(_) => (),
         }
     }
 }
@@ -185,7 +211,11 @@ impl NamespacedItem {
 fn get_function<'c>(advancement: &'c serde_json::Value) -> Option<NamespacedKey> {
     if let Some(serde_json::Value::Object(rewards)) = advancement.get("rewards") {
         if let Some(serde_json::Value::String(function)) = rewards.get("function") {
-            return NamespacedKey::from_str(function, NamespaceType::Function);
+            if let Ok(ns_key) = NamespacedKey::from_str(function, NamespaceType::Function) {
+                return Some(ns_key);
+            } else {
+                warn!("Advancement contains unparseable reward function {}", function);
+            }
         }
     }
     None
@@ -249,12 +279,6 @@ fn load_quests(items: &mut HashMap<NamespacedKey, NamespacedItem>, dir: &str) {
 
 fn load_datapack(items: &mut HashMap<NamespacedKey, NamespacedItem>,
                  dir: &str) {
-    lazy_static! {
-        static ref RE_FUNC: Regex = Regex::new(r"function [a-z][^ :]*:[a-z][^ :]*").unwrap();
-        static ref RE_ADV: Regex = Regex::new(r"advancement (grant|revoke) .* (everything|from|only|through|until) [a-z][^ :]*:[a-z][^ :]*").unwrap();
-        static ref RE_ADV_ONLY: Regex = Regex::new(r" only [a-z][^ :]*:[a-z][^ :]*").unwrap();
-    }
-
     for entry in WalkDir::new(dir).follow_links(true) {
         if let Ok(entry) = entry {
             if entry.path().is_file() {
@@ -274,11 +298,11 @@ fn load_datapack(items: &mut HashMap<NamespacedKey, NamespacedItem>,
     }
 }
 
-fn link_quest_file_recursive(pending: &mut Vec<NamespacedKey>, value: &serde_json::value::Value) {
+fn link_quest_file_recursive(children: &mut Vec<NamespacedKey>, value: &serde_json::value::Value) {
     match value {
         serde_json::value::Value::Array(array) => {
             for item in array.iter() {
-                link_quest_file_recursive(pending, item);
+                link_quest_file_recursive(children, item);
             }
         }
         serde_json::value::Value::Object(obj) => {
@@ -286,17 +310,19 @@ fn link_quest_file_recursive(pending: &mut Vec<NamespacedKey>, value: &serde_jso
                 if key == "command" {
                     if let serde_json::value::Value::String(command) = value {
                         if let Some(key) = get_command_target_namespacedkey(command) {
-                            pending.push(key);
+                            children.push(key);
                         }
                     }
                 } else if key == "function" {
                     if let serde_json::value::Value::String(function) = value {
-                        if let Some(key) = NamespacedKey::from_str(function, NamespaceType::Function) {
-                            pending.push(key);
+                        if let Ok(key) = NamespacedKey::from_str(function, NamespaceType::Function) {
+                            children.push(key);
+                        } else {
+                            warn!("Quest file contains unparseable reward function {}", function);
                         }
                     }
                 } else {
-                    link_quest_file_recursive(pending, value);
+                    link_quest_file_recursive(children, value);
                 }
             }
         }
@@ -304,25 +330,51 @@ fn link_quest_file_recursive(pending: &mut Vec<NamespacedKey>, value: &serde_jso
     }
 }
 
-fn load_commands_file(pending: &mut Vec<NamespacedKey>, path: &str) {
+fn load_commands_file(items: &mut HashMap<NamespacedKey, NamespacedItem>, path: &str) {
     if let Ok(file) = fs::read_to_string(&path) {
         if let Ok(json) = serde_json::from_str(&file) {
             if let serde_json::value::Value::Array(array) = json {
                 for item in array.iter() {
                     if let serde_json::value::Value::Object(obj) = item {
                         if let Some(serde_json::value::Value::String(command)) = obj.get("command") {
-                            if let Some(key) = get_command_target_namespacedkey(command) {
-                                pending.push(key);
+                            if let Some(serde_json::value::Value::Array(pos)) = obj.get("pos") {
+                                if let Some(serde_json::value::Value::Number(x)) = pos.get(0) {
+                                    if let Some(serde_json::value::Value::Number(y)) = pos.get(1) {
+                                        if let Some(serde_json::value::Value::Number(z)) = pos.get(2) {
+                                            let namespace = NamespacedKey::from_command(path, &format!("{} {} {}", x, y, z));
+                                            let val = NamespacedItem::Command(Command{ path: vec!(path.to_string()), children: vec!(), data: vec!(command.to_string()) });
+                                            if let Some(existing) = items.get_mut(&namespace) {
+                                                existing.add_variant(val);
+                                            } else {
+                                                items.insert(namespace, val);
+                                            }
+                                        } else {
+                                            warn!("Invalid commands json pos: {}", path);
+                                        }
+                                    } else {
+                                        warn!("Invalid commands json pos: {}", path);
+                                    }
+                                } else {
+                                    warn!("Invalid commands json pos: {}", path);
+                                }
+                            } else {
+                                warn!("Commands json entry missing 'pos': {}", path);
                             }
+                        } else {
+                            warn!("Commands json entry missing 'command': {}", path);
                         }
+                    } else {
+                        warn!("Failed to parse commands json entry as object: {}", path);
                     }
                 }
+            } else {
+                warn!("Failed to parse file as json: {}", path);
             }
         } else {
             warn!("Failed to parse file as json: {}", path);
         }
     } else {
-        warn!("Failed to read file as string: {}", &path);
+        warn!("Failed to read file as string: {}", path);
     }
 }
 
@@ -336,16 +388,22 @@ fn get_command_target_namespacedkey(command: &str) -> Option<NamespacedKey> {
     /* Link functions to the other functions they call */
     /* Schedule function and function both have the same syntax, so one match will do */
     if let Some(func_match) = RE_FUNC.find(command) {
-        if let Some(key) = NamespacedKey::from_str(func_match.as_str().trim_start_matches("function "), NamespaceType::Function) {
+        let function = func_match.as_str().trim_start_matches("function ");
+        if let Ok(key) = NamespacedKey::from_str(function, NamespaceType::Function) {
             return Some(key);
+        } else {
+            warn!("Command contains unparseable function {}", function);
         }
     }
 
     /* Link functions to advancements */
     if let Some(adv_match) = RE_ADV.find(command) {
         if let Some(adv_only_match) = RE_ADV_ONLY.find(adv_match.as_str()) {
-            if let Some(key) = NamespacedKey::from_str(adv_only_match.as_str().trim_start_matches(" only "), NamespaceType::Advancement) {
+            let advancement = adv_only_match.as_str().trim_start_matches(" only ");
+            if let Ok(key) = NamespacedKey::from_str(advancement, NamespaceType::Advancement) {
                 return Some(key);
+            } else {
+                warn!("Command contains unparseable advancement {}", advancement);
             }
         } else {
             warn!("No support for advancement command {}", adv_match.as_str());
@@ -355,19 +413,21 @@ fn get_command_target_namespacedkey(command: &str) -> Option<NamespacedKey> {
     None
 }
 
-fn create_links(items: &mut HashMap<NamespacedKey, NamespacedItem>,
-                pending: &mut Vec<NamespacedKey>) {
+fn create_links(items: &mut HashMap<NamespacedKey, NamespacedItem>) {
 
     /* Create links between the various files */
-    for (key, val) in items.iter_mut() {
+    for (_, val) in items.iter_mut() {
         match val {
             NamespacedItem::Advancement(advancement) => {
                 /* Create reference links for advancements */
 
-                /* Link to parent advancement - mostly just interesting, not actually used */
+                /* Link to parent advancement as dependency */
+                /* TODO: Is this needed? */
                 if let Some(serde_json::Value::String(parent)) = advancement.json_data.get(0).unwrap().get("parent") {
-                    if let Some(key) = NamespacedKey::from_str(parent, NamespaceType::Advancement) {
-                        pending.push(key);
+                    if let Ok(key) = NamespacedKey::from_str(parent, NamespaceType::Advancement) {
+                        advancement.children.push(key);
+                    } else {
+                        warn!("Advancement contains unparseable parent {}", parent);
                     }
                 }
 
@@ -396,13 +456,6 @@ fn create_links(items: &mut HashMap<NamespacedKey, NamespacedItem>,
                             }
                         }
                     }
-
-                    if advancement.used {
-                        /* Advancement is used - push it and its children to the pending list */
-                        pending.push(key.clone());
-                        pending.extend(advancement.children.iter().cloned());
-                        break
-                    }
                 }
             }
             NamespacedItem::Function(function) => {
@@ -416,9 +469,16 @@ fn create_links(items: &mut HashMap<NamespacedKey, NamespacedItem>,
                 }
             }
             NamespacedItem::Quest(quest) => {
-                /* TODO: Quest is always used and has no children */
+                /* TODO: Quest is always used */
                 quest.used = true;
-                link_quest_file_recursive(pending, &quest.json_data);
+                link_quest_file_recursive(&mut quest.children, &quest.json_data);
+            }
+            NamespacedItem::Command(command) => {
+                for data in command.data.iter() {
+                    if let Some(key) = get_command_target_namespacedkey(data) {
+                        command.children.push(key);
+                    }
+                }
             }
         }
     }
@@ -434,9 +494,6 @@ fn main() -> BoxResult<()> {
     /* A map of all of the advancements and functions that have been loaded */
     let mut items: HashMap<NamespacedKey, NamespacedItem> = HashMap::new();
 
-    /* A list of all the namespaced keys that are used but have not yet had their children processed */
-    let mut pending: Vec<NamespacedKey> = Vec::new();
-
     /* Load all the arguments as datapacks */
     let mut args: Vec<String> = env::args().collect();
 
@@ -448,7 +505,7 @@ fn main() -> BoxResult<()> {
     args.remove(0);
     while let Some(arg) = args.pop() {
         if arg.ends_with(".json") {
-            load_commands_file(&mut pending, &arg)
+            load_commands_file(&mut items, &arg)
         } else {
             load_datapack(&mut items, &arg);
         }
@@ -456,7 +513,18 @@ fn main() -> BoxResult<()> {
 
     load_quests(&mut items, "/home/bmarohn/home/scriptedquests");
 
-    create_links(&mut items, &mut pending);
+    create_links(&mut items);
+
+    /*
+     * Load all the things that are used into the pending list, which contains items that are
+     * children of used items but have not yet been processed / marked as used
+     */
+    let mut pending: Vec<NamespacedKey> = Vec::new();
+    for (_, value) in items.iter() {
+        if value.is_used() {
+            pending.extend(value.get_children().iter().cloned());
+        }
+    }
 
     /* Iterate through the pending list and mark things as used */
     while pending.len() > 0 {
@@ -477,8 +545,6 @@ fn main() -> BoxResult<()> {
         }
     }
 
-    println!("\nUnused Files:");
-
     /* Collect all unused items to a vector */
     let unused_items: Vec<&NamespacedItem> = items.iter().map(| (_, val) | val).filter(| val | !val.is_used()).collect();
     let mut unused_paths: Vec<&String> = Vec::new();
@@ -487,7 +553,7 @@ fn main() -> BoxResult<()> {
     }
     unused_paths.sort_by(|a, b| b.partial_cmp(&a).unwrap());
 
-    println!("\n\nUnused objectives:");
+    println!("\nUnused Files:");
     for path in unused_paths.iter() {
         println!("{}", path);
     }
