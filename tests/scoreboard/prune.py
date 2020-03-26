d07ac3a3ccb38360a4fb6a3cdd03a579cd5c98e9
NickNackGus <NickNackGus@gmail.com>
2020-03-26 18:02:51 -0400

    Added failsafe to test in case import fails

--- tests/scoreboard/prune.py
+++ tests/scoreboard/prune.py
@@ -7,68 +7,75 @@ sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".."))
 from lib.base_test import BaseTest
 
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../utility_code"))
-from lib_py3.world import World
+loadedWorldClass = False
+try:
+    from lib_py3.world import World
+    loadedWorldClass = True
+except Exception:
+    print("Skipping: Scoreboard: Prune")
+    print("Could not import world, missing required files")
 
-class ScoreboardTest(BaseTest):
-    def __init__(self, test_name, interact_on_fail=True):
-        super().__init__(test_name, interact_on_fail)
+if loadedWorldClass:
+    class ScoreboardTest(BaseTest):
+        def __init__(self, test_name, interact_on_fail=True):
+            super().__init__(test_name, interact_on_fail)
 
-    def test(self):
-        """
-        Run the test, raising an exception on error
-        """
-        self.world = World(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../test_files/Project_Epic-region_1"))
-        self.living_uuids = self.world.entity_uuids()
-        self.scoreboard = self.world.scoreboard
+        def test(self):
+            """
+            Run the test, raising an exception on error
+            """
+            self.world = World(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../test_files/Project_Epic-region_1"))
+            self.living_uuids = self.world.entity_uuids()
+            self.scoreboard = self.world.scoreboard
 
-        # Player names cannot exceed 16 characters; UUID's are
-        # always 36 characters (4 hyphens and 32 hexadecimal digits).
+            # Player names cannot exceed 16 characters; UUID's are
+            # always 36 characters (4 hyphens and 32 hexadecimal digits).
 
-        self.old_score_count = len(self.scoreboard.all_scores)
-        self.can_prune = 0
-        self.keepable_entity_scores = 0
-        for score in self.scoreboard.all_scores:
-            owner = score.at_path("Name").value
-            if len(owner) == 36:
-                if owner not in self.living_uuids:
-                    self.can_prune += 1
-                else:
-                    self.keepable_entity_scores += 1
+            self.old_score_count = len(self.scoreboard.all_scores)
+            self.can_prune = 0
+            self.keepable_entity_scores = 0
+            for score in self.scoreboard.all_scores:
+                owner = score.at_path("Name").value
+                if len(owner) == 36:
+                    if owner not in self.living_uuids:
+                        self.can_prune += 1
+                    else:
+                        self.keepable_entity_scores += 1
 
-        if self.can_prune == 0:
-            raise ValueError("Test invalid: No scores to prune.")
+            if self.can_prune == 0:
+                raise ValueError("Test invalid: No scores to prune.")
 
-        if self.keepable_entity_scores == 0:
-            raise ValueError("Test invalid: No entity scores would be kept.")
+            if self.keepable_entity_scores == 0:
+                raise ValueError("Test invalid: No entity scores would be kept.")
 
-        self.scoreboard.prune_missing_entities(self.living_uuids)
+            self.scoreboard.prune_missing_entities(self.living_uuids)
 
-        self.new_score_count = len(self.scoreboard.all_scores)
+            self.new_score_count = len(self.scoreboard.all_scores)
 
-        self.not_pruned = 0
-        for score in self.scoreboard.all_scores:
-            owner = score.at_path("Name").value
-            if (
-                len(owner) == 36 and
-                owner not in self.living_uuids
-            ):
-                self.not_pruned += 1
+            self.not_pruned = 0
+            for score in self.scoreboard.all_scores:
+                owner = score.at_path("Name").value
+                if (
+                    len(owner) == 36 and
+                    owner not in self.living_uuids
+                ):
+                    self.not_pruned += 1
 
-        if self.not_pruned > 0:
-            raise ValueError("{} scores not pruned.".format(self.not_pruned))
+            if self.not_pruned > 0:
+                raise ValueError("{} scores not pruned.".format(self.not_pruned))
 
-        if self.new_score_count + self.can_prune > self.old_score_count:
-            raise ValueError("Not enough scores were pruned.")
+            if self.new_score_count + self.can_prune > self.old_score_count:
+                raise ValueError("Not enough scores were pruned.")
 
-        if self.new_score_count + self.can_prune < self.old_score_count:
-            raise ValueError("Too many scores were pruned.")
+            if self.new_score_count + self.can_prune < self.old_score_count:
+                raise ValueError("Too many scores were pruned.")
 
-    def debug(self):
-        """
-        Provide extra debug info on failure
-        """
-        pass
+        def debug(self):
+            """
+            Provide extra debug info on failure
+            """
+            pass
 
-reset_test = ScoreboardTest("Scoreboard: Prune entities")
-reset_test.run()
+    reset_test = ScoreboardTest("Scoreboard: Prune entities")
+    reset_test.run()
 
