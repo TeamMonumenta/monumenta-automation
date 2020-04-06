11b5ebdfb9cfb5e7e70a2f4022cff9fb0950ec03
Byron Marohn <combustible@live.com>
2020-04-05 23:27:14 -0400

    Make print_player_nbt example actually runnable
    Signed-off-by: Byron Marohn <combustible@live.com>

--- utility_code/examples/print_player_nbt.py
+++ utility_code/examples/print_player_nbt.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".."))
 from lib_py3.player import Player
+
 
 player = Player("/home/epic/play/project_epic/lightgray/Project_Epic-lightgray/playerdata/f374dfda-9760-4d74-aae8-7be7f61294b5.dat")
 
