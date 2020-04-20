4a163011714e4bb2979462b9270f8a81f01b4d37
Byron Marohn <combustible@live.com>
2020-04-20 01:14:32 +0000

    Rust: Add tool to update uuid2name/name2uuid indexes
    Signed-off-by: Byron Marohn <combustible@live.com>

---
+++ rust/src/bin/update_redis_uuid2name_name2uuid.rs
@@ -0,0 +1,47 @@
+use std::error::Error;
+type BoxResult<T> = Result<T,Box<dyn Error>>;
+
+use redis::Commands;
+use std::env;
+use std::collections::HashMap;
+use simplelog::*;
+
+use monumenta::player::Player;
+
+fn main() -> BoxResult<()> {
+    CombinedLogger::init(
+        vec![
+            TermLogger::new(LevelFilter::Debug, Config::default(), TerminalMode::Mixed).unwrap(),
+        ]
+    ).unwrap();
+
+    let mut args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("Usage: save_playerdata_to_redis path/to/directory");
+        return Ok(());
+    }
+
+    args.remove(0);
+
+    let domain = args.remove(0);
+
+    let mut uuid2name: HashMap<String, String> = HashMap::new();
+
+    let client = redis::Client::open("redis://127.0.0.1/")?;
+    let mut con : redis::Connection = client.get_connection()?;
+
+    for (uuid, player) in Player::get_redis_players(&domain, &mut con)?.iter_mut() {
+        player.load_redis(&domain, &mut con)?;
+        if let Some(name) = &player.name {
+            let uuidstr = uuid.to_hyphenated().to_string();
+            let _: () = con.hset("uuid2name", &uuidstr, name)?;
+            let _: () = con.hset("name2uuid", name, &uuidstr)?;
+            uuid2name.insert(uuidstr.to_string(), name.to_string());
+        }
+    }
+
+    println!("\n\nuuid2name:{:?}", uuid2name);
+
+    Ok(())
+}
