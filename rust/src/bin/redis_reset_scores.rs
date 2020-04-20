a960283ad02bd6bd4f53b1ebe88e4e2f9e951b3d
Byron Marohn <combustible@live.com>
2020-04-20 04:51:12 +0000

    Rust: Add a utility to reset player scores for an objective
    Signed-off-by: Byron Marohn <combustible@live.com>

---
+++ rust/src/bin/redis_reset_scores.rs
@@ -0,0 +1,41 @@
+use std::error::Error;
+type BoxResult<T> = Result<T,Box<dyn Error>>;
+
+use std::env;
+use simplelog::*;
+
+use monumenta::player::Player;
+
+fn main() -> BoxResult<()> {
+    CombinedLogger::init(
+        vec![
+            TermLogger::new(LevelFilter::Debug, Config::default(), TerminalMode::Mixed).unwrap(),
+        ]
+    ).unwrap();
+
+    let mut args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        println!("Usage: redis_reset_scores <domain> <objective>");
+        return Ok(());
+    }
+
+    args.remove(0);
+
+    let domain = args.remove(0);
+    let objective = args.remove(0);
+
+    let client = redis::Client::open("redis://127.0.0.1/")?;
+    let mut con : redis::Connection = client.get_connection()?;
+
+    for (_, player) in Player::get_redis_players(&domain, &mut con)?.iter_mut() {
+        player.load_redis(&domain, &mut con)?;
+        if let Some(scores) = &mut player.scores {
+            scores.insert(objective.to_string(), 0);
+            player.save_redis(&domain, &mut con, &format!("Reset scores for {}", &objective))?;
+        }
+        println!("{}", player);
+    }
+
+    Ok(())
+}
