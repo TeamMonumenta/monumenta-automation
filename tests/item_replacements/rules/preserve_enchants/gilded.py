5d217a823c1ace53449df0c028f4973a0f64dd6c
Tim Southwick <NickNackGus@gmail.com>
2020-06-01 17:41:19 -0400

    Updated tests mentioning the Zombie Plushie

--- tests/item_replacements/rules/preserve_enchants/gilded.py
+++ tests/item_replacements/rules/preserve_enchants/gilded.py
@@ -9,9 +9,9 @@ from lib.item_test import ItemTest
 
 test_item = ItemTest(
     test_name="Rules: Preserve Gilded",
-    template_item=r'''{id:"minecraft:zombie_head",tag:{display:{Lore:["§8King's Valley : Tier II"],Name:"{\"text\":\"§rZombie Plushy\"}"},Enchantments:[{lvl:1s,id:"minecraft:protection"}],AttributeModifiers:[]}}''',
-    item_under_test=r'''{id:"minecraft:zombie_head",tag:{display:{Lore:["§7Gilded","§8King's Valley : Tier II","Gilded by Foo_Bar_Baz"],Name:"{\"text\":\"§rZombie Plushy\"}"},Enchantments:[{lvl:1s,id:"minecraft:protection"}],AttributeModifiers:[]}}''',
-    expected_result_item=r'''{id:"minecraft:zombie_head",tag:{display:{Lore:["§7Gilded","§8King's Valley : Tier II","Gilded by Foo_Bar_Baz"],Name:"{\"text\":\"§rZombie Plushy\"}"},Enchantments:[{lvl:1s,id:"minecraft:protection"}],AttributeModifiers:[]}}'''
+    template_item=r'''{id:"minecraft:zombie_head",tag:{display:{Lore:["§8King's Valley : Tier II"],Name:"{\"text\":\"§rZombie Plushie\"}"},Enchantments:[{lvl:1s,id:"minecraft:protection"}],AttributeModifiers:[]}}''',
+    item_under_test=r'''{id:"minecraft:zombie_head",tag:{display:{Lore:["§7Gilded","§8King's Valley : Tier II","Gilded by Foo_Bar_Baz"],Name:"{\"text\":\"§rZombie Plushie\"}"},Enchantments:[{lvl:1s,id:"minecraft:protection"}],AttributeModifiers:[]}}''',
+    expected_result_item=r'''{id:"minecraft:zombie_head",tag:{display:{Lore:["§7Gilded","§8King's Valley : Tier II","Gilded by Foo_Bar_Baz"],Name:"{\"text\":\"§rZombie Plushie\"}"},Enchantments:[{lvl:1s,id:"minecraft:protection"}],AttributeModifiers:[]}}'''
 )
 test_item.run()
 
