2c3d0282e82c6fa17664cf36f771547e9887bf1e
Byron Marohn <combustible@live.com>
2020-06-02 00:43:30 -0400

    Update quarry and improve tests parsing mojangson integer arrays
    Signed-off-by: Byron Marohn <combustible@live.com>

--- tests/test_mojangson_parsing.py
+++ tests/test_mojangson_parsing.py
@@ -19,7 +19,10 @@ def test_fail(test_str):
 
 def test(test_str):
     print(nbt.TagCompound.from_mojangson(test_str))
-    nbt.TagCompound.from_mojangson(test_str).tree()
+    val = nbt.TagCompound.from_mojangson(test_str)
+    nop = val.to_mojangson()
+    nop = val.to_bytes()
+    val.tree()
 
 print("\n\nFail test 1")
 test_fail('''x''')
@@ -28,23 +31,24 @@ test_fail('''{xaoeu''')
 print("\n\nFail test 3")
 test_fail('''{xaoeu:w''')
 
-# Simple test
 print("\n\nBasic compound test")
 test(r'''{xaoeu:"aoeu"}''')
-# Item test
+
 print("\n\nItem test")
 test(r'''{display:{Lore:["§9Cloth Armor","§8King's Valley : §6Patron Made","§1§oA cobalt blue shift"],color:37355,Name:"{\"text\":\"§3§lCobaltean Cape\"}"},Enchantments:[{lvl:2s,id:"minecraft:feather_falling"},{lvl:1s,id:"minecraft:mending"},{lvl:1s,id:"minecraft:protection"},{lvl:1s,id:"minecraft:fire_protection"}],AttributeModifiers:[{UUIDMost:-6264657341144218168L,UUIDLeast:-8082316997105348303L,Amount:1.5d,Slot:"chest",AttributeName:"generic.armor",Operation:0,Name:"Modifier"},{UUIDMost:3502922254951270786L,UUIDLeast:-6810533993790190373L,Amount:-0.1d,Slot:"chest",AttributeName:"generic.maxHealth",Operation:1,Name:"Modifier"},{UUIDMost:-5333876828708518262L,UUIDLeast:-6396568730443630460L,Amount:0.18d,Slot:"chest",AttributeName:"generic.movementSpeed",Operation:1,Name:"Modifier"}]}''')
-# Item test with spaces
+
 print("\n\nItem test with spaces")
 test(r'''{display: {Lore: ["§9Cloth Armor", "§8King's Valley : §6Patron Made", "§1§oA cobalt blue shift"], color: 37355, Name: "{\"text\":\"§3§lCobaltean Cape\"}"}, Enchantments: [{lvl: 1s, id: "minecraft:protection"}, {lvl: 1s, id: "minecraft:fire_protection"}, {lvl: 2s, id: "minecraft:feather_falling"}, {lvl: 1s, id: "minecraft:mending"}], Damage: 0, AttributeModifiers: [{UUIDMost: -6264657341144218168L, UUIDLeast: -8082316997105348303L, Amount: 1.5d, Slot: "chest", AttributeName: "generic.armor", Operation: 0, Name: "Modifier"}, {UUIDMost: 3502922254951270786L, UUIDLeast: -6810533993790190373L, Amount: -0.1d, Slot: "chest", AttributeName: "generic.maxHealth", Operation: 1, Name: "Modifier"}, {UUIDMost: -5333876828708518262L, UUIDLeast: -6396568730443630460L, Amount: 0.18d, Slot: "chest", AttributeName: "generic.movementSpeed", Operation: 1, Name: "Modifier"}]}''')
 
-# Firework test
 print("\n\nFirework test")
 test(r'''{display:{Lore:["§9To signal that you're","§9ready to party!"],Name:"{\"text\":\"§5§lSignal Flare - Party Rocket\"}"},Fireworks:{Flight:1b,Explosions:[{Type:2b,Colors:[I;16711680],FadeColors:[I;16738063]},{Type:2b,Colors:[I;526591],FadeColors:[I;15138563]},{Type:2b,Colors:[I;1310467],FadeColors:[I;754951]}]}}''')
 
-# Book test / nested quotes
 print("\n\nBook test / nested quotes")
 test(r'''{display:{Name:'{"text":"§2§lNecronomicon"}',Lore:['{"text":"§8Monumenta : §6Patron Made"}','{"text":"§8The lexicon of forbidden knowledge comes"}','{"text":"§8in many forms, some are even cute."}','{"text":"§r"}','{"text":"§7When in main hand:"}','{"text":"§eRight click to spend 10 levels worth"}','{"text":"§eof EXP and remove Poison and Nausea."}']}}''')
+
+print("\n\nFrom mojangson -> to bytes")
+test(r'''{display:{Lore:["{\"text\":\"\\u00a78Celsian Isles : \\u00a75Unique\"}","{\"text\":\"\\u00a78A firework enchanted by Ebonee.\"}"],Name:"{\"text\":\"§e§lBewitched Firework\"}"},Enchantments:[{lvl:1s,id:"minecraft:flame"}],Fireworks:{Explosions:[{Type:2b,Trail:1b,Colors:[I;14706431],Flicker:1b,FadeColors:[I;8913151]}]}}''')
+
 
 # Test that serialization is reverse of deserialization
 print("\n\nSerialize/Deserialize test:")
