3a6dbbb414f2e3da4cbd7ff4bda975c4f54861cc
Byron Marohn <combustible@live.com>
2020-06-06 01:03:21 -0400

    Implement command_block_update_tool loading
    Signed-off-by: Byron Marohn <combustible@live.com>

--- utility_code/command_block_update_tool.py
+++ utility_code/command_block_update_tool.py
@@ -7,7 +7,7 @@ import code
 import sys
 import getopt
 import json
-from pprint import pprint
+from pprint import pprint, pformat
 
 from lib_py3.common import eprint
 from lib_py3.world import World
@@ -110,9 +110,40 @@ else:
         sys.exit("--pos1 and --pos2 only make sense with output mode")
 
     # Load the data
-    data = []
+    # key = "x y z"
+    # value = { pos/command/name/auto/powered }
+    commands = {}
     with open(input_path, 'r') as outfile:
         data = json.load(outfile)
+        for element in data:
+            if "pos" in element:
+                key = "{} {} {}".format(element["pos"][0], element["pos"][1], element["pos"][2])
+                commands[key] = element
+            else:
+                eprint("Warning: Skipping command without position: " + pformat(element))
 
-    #TODO
-    raise NotImplementedError("ERROR WIP")
+    for entity, source_pos, entity_path in world.entity_iterator(pos1=pos1, pos2=pos2, readonly=False):
+        if source_pos is not None and entity.has_path('Command'):
+            key = "{} {} {}".format(source_pos[0], source_pos[1], source_pos[2])
+            command_to_load = commands.get(key, None)
+            if command_to_load is not None:
+                entity.at_path('Command').value = command_to_load["command"]
+
+            # TODO: This stuff was used to restore commands and upgrade stuff from 1.13. It's probably not needed anymore
+            # from update_lore_everywhere import process_one_command
+            # command_to_load = commands.get(key, None)
+            # if command_to_load is not None:
+            #     if not entity.at_path('Command').value.strip() and command_to_load["command"].strip():
+            #         entity.at_path('Command').value = command_to_load["command"]
+            #         print("")
+            #         print("Command at {} is empty, restored to '{}'".format(key, command_to_load["command"]))
+            #
+            # orig_command = entity.at_path('Command').value
+            # updated_command = process_one_command(orig_command)
+            # if updated_command is not None:
+            #     print("")
+            #     print("LOC : " + key)
+            #     print("ORIG: " + orig_command)
+            #     print("NEW : " + updated_command)
+            #     entity.at_path('Command').value = updated_command
+
