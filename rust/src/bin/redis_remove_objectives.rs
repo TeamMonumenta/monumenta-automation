21da6cc190d7c98f06170a5beecc29c429dbc93d
Byron Marohn <combustible@live.com>
2020-10-08 23:07:39 +0000

    Add tool to remove scoreboard objectives from redis players
    Signed-off-by: Byron Marohn <combustible@live.com>

---
+++ rust/src/bin/redis_remove_objectives.rs
@@ -0,0 +1,67 @@
+use std::error::Error;
+type BoxResult<T> = Result<T,Box<dyn Error>>;
+
+use std::env;
+use simplelog::*;
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+
+use monumenta::player::Player;
+
+fn main() -> BoxResult<()> {
+    let mut multiple = vec![];
+    match TermLogger::new(LevelFilter::Debug, Config::default(), TerminalMode::Mixed) {
+        Some(logger) => multiple.push(logger as Box<dyn SharedLogger>),
+        None => multiple.push(SimpleLogger::new(LevelFilter::Debug, Config::default())),
+    }
+    CombinedLogger::init(multiple).unwrap();
+
+    let mut args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        println!("Usage: redis_reset_scores <domain> <objectives_list_file>");
+        return Ok(());
+    }
+
+    args.remove(0);
+
+    let domain = args.remove(0);
+    let objectives_file = args.remove(0);
+    let mut objectives = vec!();
+
+    let file = File::open(objectives_file)?;
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        let mut objective = line?;
+        objective.retain(|c| !c.is_whitespace());
+        objectives.push(objective);
+    }
+
+    println!("Objectives to remove:");
+    for objective in &objectives {
+        println!("  {}", objective);
+    }
+
+    let client = redis::Client::open("redis://127.0.0.1/")?;
+    let mut con : redis::Connection = client.get_connection()?;
+
+    println!("Removing objectives...");
+
+    let mut num_players = 0;
+    for (_, player) in Player::get_redis_players(&domain, &mut con)?.iter_mut() {
+        player.load_redis(&domain, &mut con)?;
+        if let Some(scores) = &mut player.scores {
+            for objective in &objectives {
+                scores.remove(objective);
+            }
+            player.update_history(&format!("Removed {} scoreboard objectives", objectives.len()));
+            player.save_redis(&domain, &mut con)?;
+            num_players = num_players + 1;
+        }
+    }
+
+    println!("Removed {} objectives from {} players", objectives.len(), num_players);
+
+    Ok(())
+}
