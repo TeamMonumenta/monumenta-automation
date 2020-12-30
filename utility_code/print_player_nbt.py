8b9a683395cc3e330fb9b9345d301ad89e612e7f
Byron Marohn <combustible@live.com>
2020-12-29 21:21:34 -0500

    Upgarde print_player_nbt.py to minecraft library
    Signed-off-by: Byron Marohn <combustible@live.com>

--- utility_code/print_player_nbt.py
+++ utility_code/print_player_nbt.py
@@ -2,14 +2,14 @@
 
 import sys
 import os
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".."))
-from lib_py3.player import Player
+
+from minecraft.player_dat_format.player import PlayerFile
 
 args = sys.argv
 
 if len(args) != 2:
     print("Usage: print_player_nbt </path/to/player.dat>")
 else:
-    player = Player(args[1])
+    player = PlayerFile(args[1]).player
 
-    player.player_tag.tree()
+    player.nbt.tree()
