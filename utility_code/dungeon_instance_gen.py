67f257a42fba93f7fe2acd193b699340d62aad50
Byron Marohn <combustible@live.com>
2021-04-15 21:15:40 -0400

    Increase willows instances
    Signed-off-by: Byron Marohn <combustible@live.com>

--- utility_code/dungeon_instance_gen.py
+++ utility_code/dungeon_instance_gen.py
@@ -83,7 +83,7 @@ config = {
         },
         "willows":{
             "region":{"x":-3, "z":3},
-            "count":40,
+            "count":50,
             "objective":"DB1Access"
         },
         "roguelike":{
