fd3986e56867bd67461a55ded4a164a30a365373
Byron Marohn <combustible@live.com>
2021-11-24 15:19:36 -0500

    More WIP plot migration scripts
    Signed-off-by: Byron Marohn <combustible@live.com>

---
+++ utility_code/plot_migrate_gen.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import json
+from queue import Empty
+
+from minecraft.world import World
+from minecraft.player_dat_format.item import Item
+from multiprocessing import Process, Queue
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../quarry"))
+from quarry.types import nbt
+
+def get_mailbox_chests(world, read_only=True):
+    for region in world.iter_regions(min_x = -1292, min_y = 101, min_z = -1292, max_x = -1292 + 1, max_y = 101 + 2, max_z = -1292 + 1, read_only=read_only):
+        for chunk in region.iter_chunks(min_x = -1292, min_y = 101, min_z = -1292, max_x = -1292 + 1, max_y = 101 + 2, max_z = -1292 + 1, autosave=not read_only):
+            for block_entity in chunk.iter_block_entities(min_x = -1292, min_y = 101, min_z = -1292, max_x = -1292 + 1, max_y = 101 + 2, max_z = -1292 + 1):
+                yield block_entity
+
+def process(inq, outq):
+    template_world = World("template")
+    template_filled_world = World("templatefilled")
+
+    try:
+        while not inq.empty():
+            plot = inq.get(block=False)
+            world_id = plot["world_id"]
+            if plot["border_item_frames"]:
+                world = template_filled_world.copy_to(f"plot{world_id}")
+            else:
+                world = template_world.copy_to(f"plot{world_id}")
+
+            items = plot["mailbox_items"]
+            if len(items) > 0:
+                for block_entity in get_mailbox_chests(world, read_only=False):
+                    if block_entity.id != "minecraft:chest":
+                        print(f"WARNING: Got unexpected block entity {block_entity} at -1292 102 -1292 in world plot{world_id}")
+                        continue
+
+                    # Only chests here
+
+                    slot = 0
+                    while slot < 27 and len(items) > 0:
+                        item_nbt = nbt.TagCompound.from_mojangson(items.pop())
+                        item_nbt.value["Slot"] = nbt.TagByte(slot)
+                        block_entity.nbt.at_path("Items").value.append(item_nbt)
+                        slot += 1
+
+            if len(items) > 0:
+                print(f"WARNING: Still had {len(items)} mailbox items left after populating mailbox in world plot{world_id}")
+
+            outq.put(plot["world_id"])
+    except Empty:
+        pass
+
+
+# Get the plot command blocks and their orientations
+with open("all_plot_records_with_mail.json", "r") as fp:
+    plots = json.load(fp)
+
+inq = Queue()
+outq = Queue()
+count = 0
+for plot in plots:
+    inq.put(plots[plot])
+    count += 1
+    if count > 0: # TODO: Remove this
+        break
+
+threads = 4
+if threads > 1:
+    procs = []
+    for i in range(0, threads):
+        p = Process(target=process, args=(inq,outq))
+        p.start()
+        procs.append(p)
+
+    for proc in procs:
+        proc.join()
+else:
+    process(inq, outq)
+
+while not outq.empty():
+    print(outq.get(block=False))
+
---
+++ utility_code/plot_migrate_mailboxes.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+import sys
+import math
+
+from collections import OrderedDict
+from lib_py3.redis_scoreboard import RedisScoreboard
+from r1plot_lookup import lut
+from pprint import pprint
+from minecraft.world import World
+import json
+
+def get_items(world, min_x, min_y, min_z, max_x, max_y, max_z):
+    items = []
+    #print(min_x, min_y, min_z, max_x, max_y, max_z)
+    for region in world.iter_regions(min_x = min_x, min_y = min_y, min_z = min_z, max_x = max_x, max_y = max_y, max_z = max_z, read_only=True):
+        for chunk in region.iter_chunks(min_x = min_x, min_y = min_y, min_z = min_z, max_x = max_x, max_y = max_y, max_z = max_z, autosave=False):
+            for block_entity in chunk.iter_block_entities(min_x = min_x, min_y = min_y, min_z = min_z, max_x = max_x, max_y = max_y, max_z = max_z):
+                #print(block_entity.pos)
+                for item in block_entity.iter_items():
+                    #print(item.id)
+                    items.append(item.nbt.to_mojangson())
+
+    return items
+
+
+# Get the plot command blocks and their orientations
+with open("all_plot_records.json", "r") as fp:
+    plots = json.load(fp)
+
+world = World("/home/epic/stage/m12/plots/Project_Epic-plots")
+
+for plot_id in plots:
+    plot = plots[plot_id]
+
+    min_x = plot["min"][0]
+    min_y = plot["min"][1]
+    min_z = plot["min"][2]
+    max_x = plot["max"][0]
+    max_y = plot["max"][1]
+    max_z = plot["max"][2]
+
+    plot["border_item_frames"] = False
+    for region in world.iter_regions(min_x = min_x, min_y = min_y, min_z = min_z, max_x = max_x + 1, max_y = max_y + 1, max_z = max_z + 1, read_only=True):
+        for chunk in region.iter_chunks(min_x = min_x, min_y = min_y, min_z = min_z, max_x = max_x + 1, max_y = max_y + 1, max_z = max_z + 1, autosave=False):
+            for entity in chunk.iter_entities(min_x = min_x, min_y = min_y, min_z = min_z, max_x = max_x + 1, max_y = max_y + 1, max_z = max_z + 1):
+                if "item_frame" in entity.id:
+                    x = math.floor(entity.pos[0])
+                    y = math.floor(entity.pos[1])
+                    z = math.floor(entity.pos[2])
+                    if (x == min_x or x == min_y or z == min_z or z == max_z or y == max_y):
+                        plot["border_item_frames"] = True
+
+    facing = plot["facing"]
+    # The max arguments are exclusive for entity / block entities!
+    if facing == "north":
+        plot["mailbox_items"] = get_items(world, max_x - 1, min_y + 10, max_z + 1, max_x, min_y + 18, max_z + 3)
+    elif facing == "east":
+        plot["mailbox_items"] = get_items(world, min_x - 2, min_y + 10, max_z - 1, min_x, min_y + 18, max_z)
+    elif facing == "south":
+        plot["mailbox_items"] = get_items(world, min_x + 1, min_y + 10, min_z - 2, min_x + 2, min_y + 18, min_z)
+    elif facing == "west":
+        plot["mailbox_items"] = get_items(world, max_x + 1, min_y + 10, min_z + 1, max_x + 3, min_y + 18, min_z + 2)
+
+#    if plot["world_id"] == 1:
+#        pprint(plot)
+#        break
+
+with open("all_plot_records_with_mail.json", "w") as fp:
+    json.dump(plots, fp, ensure_ascii=False, sort_keys=False, indent=2, separators=(',', ': '))
