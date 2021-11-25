06baabdcefe31c61ff6f21302af25111cddbac73
Byron Marohn <combustible@live.com>
2021-11-25 02:41:25 -0500

    Grab chests & entities in plot doorways
    Signed-off-by: Byron Marohn <combustible@live.com>

--- utility_code/plot_migrate_mailboxes.py
+++ utility_code/plot_migrate_mailboxes.py
@@ -22,6 +22,16 @@ def get_items(world, min_x, min_y, min_z, max_x, max_y, max_z):
 
     return items
 
+def get_entities(world, min_x, min_y, min_z, max_x, max_y, max_z):
+    entities = []
+    #print(min_x, min_y, min_z, max_x, max_y, max_z)
+    for region in world.iter_regions(min_x = min_x, min_y = min_y, min_z = min_z, max_x = max_x, max_y = max_y, max_z = max_z, read_only=True):
+        for chunk in region.iter_chunks(min_x = min_x, min_y = min_y, min_z = min_z, max_x = max_x, max_y = max_y, max_z = max_z, autosave=False):
+            for entity in chunk.iter_entities(min_x = min_x, min_y = min_y, min_z = min_z, max_x = max_x, max_y = max_y, max_z = max_z):
+                entities.append(entity.nbt.to_mojangson())
+
+    return entities
+
 
 # Get the plot command blocks and their orientations
 with open("all_plot_records.json", "r") as fp:
@@ -53,15 +63,19 @@ for plot_id in plots:
     facing = plot["facing"]
     # The max arguments are exclusive for entity / block entities!
     if facing == "north":
-        plot["mailbox_items"] = get_items(world, max_x - 1, min_y + 10, max_z + 1, max_x, min_y + 18, max_z + 3)
+        plot["mailbox_items"] = get_items(world, max_x - 11, min_y + 10, max_z + 1, max_x, min_y + 21, max_z + 3)
+        plot["door_entities"] = get_entities(world, max_x - 11, min_y + 10, max_z + 1, max_x, min_y + 21, max_z + 3)
     elif facing == "east":
-        plot["mailbox_items"] = get_items(world, min_x - 2, min_y + 10, max_z - 1, min_x, min_y + 18, max_z)
+        plot["mailbox_items"] = get_items(world, min_x - 2, min_y + 10, max_z - 11, min_x, min_y + 21, max_z)
+        plot["door_entities"] = get_entities(world, min_x - 2, min_y + 10, max_z - 11, min_x, min_y + 21, max_z)
     elif facing == "south":
-        plot["mailbox_items"] = get_items(world, min_x + 1, min_y + 10, min_z - 2, min_x + 2, min_y + 18, min_z)
+        plot["mailbox_items"] = get_items(world, min_x + 1, min_y + 10, min_z - 2, min_x + 12, min_y + 21, min_z)
+        plot["door_entities"] = get_entities(world, min_x + 1, min_y + 10, min_z - 2, min_x + 12, min_y + 21, min_z)
     elif facing == "west":
-        plot["mailbox_items"] = get_items(world, max_x + 1, min_y + 10, min_z + 1, max_x + 3, min_y + 18, min_z + 2)
+        plot["mailbox_items"] = get_items(world, max_x + 1, min_y + 10, min_z + 1, max_x + 3, min_y + 21, min_z + 12)
+        plot["door_entities"] = get_entities(world, max_x + 1, min_y + 10, min_z + 1, max_x + 3, min_y + 21, min_z + 12)
 
-#    if plot["world_id"] == 1:
+#    if plot["world_id"] == 8:
 #        pprint(plot)
 #        break
 
