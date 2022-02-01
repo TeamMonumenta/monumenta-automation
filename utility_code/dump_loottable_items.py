c7cc88605caf0af21e5831c2da1c167fbc431495
Timothy Southwick <NickNackGus@gmail.com>
2022-02-01 15:02:17 -0500

    pylint improvements

--- utility_code/dump_loottable_items.py
+++ utility_code/dump_loottable_items.py
@@ -5,23 +5,32 @@ import argparse
 import os
 import sys
 import json
-import re
-from collections import OrderedDict
-from pprint import pprint
 
 from lib_py3.loot_table_manager import LootTableManager
-from lib_py3.upgrade import upgrade_entity
-from lib_py3.common import parse_name_possibly_json
 
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../quarry"))
 from quarry.types import nbt
-from quarry.types.text_format import unformat_text
 
 
-def is_up_to_date(item_id, plain_name, item_tag):
+def is_up_to_date(item_tag):
+    """Returns true if an item is up to date."""
     if item_tag.has_path("Monumenta"):
         return True
     return False
+
+
+def print_give_chest(chest_count, container_list):
+    chest_name_json = json.dumps({"text":str(chest_count)})
+    chest_tag = nbt.TagCompound({
+        "BlockEntityTag": nbt.TagCompound({
+            "Items": nbt.TagList(container_list),
+            "id": nbt.TagString("minecraft:chest")
+        }),
+        "display": nbt.TagCompound({
+            "Name": nbt.TagString(chest_name_json)
+        })
+    })
+    print('give @s chest' + chest_tag.to_mojangson())
 
 
 def main():
@@ -36,7 +45,7 @@ def main():
     chestcount = 0
     for item_id in mgr.item_map:
         for item in mgr.item_map[item_id]:
-            if type(mgr.item_map[item_id][item]) is list:
+            if isinstance(mgr.item_map[item_id][item], list):
                 entry = mgr.item_map[item_id][item][0]
             else:
                 entry = mgr.item_map[item_id][item]
@@ -44,18 +53,23 @@ def main():
                 # Skip generated entries
                 continue
             item_nbt = entry["nbt"]
-            if args.unupdated and is_up_to_date(item_id, item, item_nbt):
+            if args.unupdated and is_up_to_date(item_nbt):
                 continue
 
-            item_slot = nbt.TagCompound({"Slot": nbt.TagByte(len(containerlist)), "Count": nbt.TagByte(1), "id": nbt.TagString(item_id), "tag": item_nbt})
+            item_slot = nbt.TagCompound({
+                "Slot": nbt.TagByte(len(containerlist)),
+                "Count": nbt.TagByte(1),
+                "id": nbt.TagString(item_id),
+                "tag": item_nbt
+            })
             containerlist.append(item_slot)
             if len(containerlist) == 27:
                 chestcount += 1
-                print(r'''give @s chest{display:{Name:"\"''' + str(chestcount) + r'''\""},''' + nbt.TagCompound({"id": nbt.TagString("minecraft:chest"), "BlockEntityTag": nbt.TagCompound({"Items": nbt.TagList(containerlist)})}).to_mojangson()[1:])
+                print_give_chest(chestcount, containerlist)
                 containerlist = []
 
     chestcount += 1
-    print(r'''give @s chest{display:{Name:"\"''' + str(chestcount) + r'''\""},''' + nbt.TagCompound({"id": nbt.TagString("minecraft:chest"), "BlockEntityTag": nbt.TagCompound({"Items": nbt.TagList(containerlist)})}).to_mojangson()[1:])
+    print_give_chest(chestcount, containerlist)
     containerlist = []
 
 
