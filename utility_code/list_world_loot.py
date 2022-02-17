558f15566000236c8dcc093c4272b909232de8fc
Byron Marohn <combustible@live.com>
2022-02-17 14:43:24 -0500

    Improvements to list_world_loot.py
    Signed-off-by: Byron Marohn <combustible@live.com>

--- utility_code/list_world_loot.py
+++ utility_code/list_world_loot.py
@@ -1,15 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/env pypy3
 
 """List items and loot tables in a world, with an optional search area."""
 
 import argparse
 import math
 import sys
-from collections import Counter
 from pathlib import Path
+import yaml
 
-from lib_py3.common import eprint
-
+from lib_py3.common import parse_name_possibly_json
 from minecraft.chunk_format.block_entity import BlockEntity
 from minecraft.chunk_format.entity import Entity
 from minecraft.player_dat_format.item import Item
@@ -18,6 +17,8 @@ from minecraft.world import World
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(description=__doc__)
     arg_parser.add_argument('world', type=Path, help="A world folder")
+    arg_parser.add_argument('--trades-only', action='store_true')
+    arg_parser.add_argument('--logfile', type=Path, help="Log file to write to, defaults to stdout", nargs='?', default=sys.stdout)
     arg_parser.add_argument('x1', type=int, nargs='?')
     arg_parser.add_argument('y1', type=int, nargs='?')
     arg_parser.add_argument('z1', type=int, nargs='?')
@@ -56,32 +57,40 @@ if __name__ == '__main__':
         max_y += 1
         max_z += 1
 
-    loot_tables = []
-    items_found = []
+    loot_tables = {}
+    items_found = {}
 
     world = World(str(args.world))
     for region in world.iter_regions(min_x, min_y, min_z, max_x, max_y, max_z, read_only=True):
-        for chunk in region.iter_chunks(min_x, min_y, min_z, max_x, max_y, max_z):
+        for chunk in region.iter_chunks(min_x, min_y, min_z, max_x, max_y, max_z, autosave=False):
             for thing in chunk.recursive_iter_all_types(min_x, min_y, min_z, max_x, max_y, max_z):
-                if isinstance(thing, Item):
-                    items_found.append(thing.get_debug_str())
+                if thing.parent is not None and (not args.trades_only or (isinstance(thing.parent, Entity) and (thing.parent.id == "villager" or thing.parent.id == "minecraft:villager"))):
+                    parent_path = thing.parent.get_debug_str()
+                    if isinstance(thing, Item) and thing.id != "minecraft:air":
+                        locmap = items_found.get(parent_path, {})
+                        name = ""
+                        if thing.nbt.has_path("tag.display.Name"):
+                            name = parse_name_possibly_json(thing.nbt.at_path("tag.display.Name").value, remove_color=True) + "   " + thing.id
+                        lst = locmap.get(name, [])
+                        lst.append(thing.nbt.to_mojangson())
+                        locmap[name] = lst
+                        items_found[parent_path] = locmap
 
-                elif isinstance(thing, (Entity, BlockEntity)):
-                    if thing.nbt.has_path("LootTable"):
-                        loot_table = thing.nbt.at_path("LootTable").value
-                        pos = " ".join([str(x) for x in thing.pos])
-                        loot_tables.append(f"{pos} {loot_table}")
+                    elif isinstance(thing, (Entity, BlockEntity)):
+                        if thing.nbt.has_path("LootTable"):
+                            lst = loot_tables.get(parent_path, [])
+                            lst.append(thing.nbt.at_path("LootTable").value)
+                            loot_tables[parent_path] = lst
 
-    if loot_tables:
-        print("Loot table(s):")
-        for loot_table in loot_tables:
-            print(loot_table)
-    else:
-        print("No loot tables found.")
+    outpath = 'stdout' if args.logfile is sys.stdout else args.logfile
 
-    if items_found:
-        print("Item(s):")
-        for item in items_found:
-            print(item)
-    else:
-        print("No items found.")
+    if args.logfile is not sys.stdout:
+        args.logfile = open(args.logfile, "w")
+
+    output = {"loot_tables": loot_tables, "items": items_found}
+    yaml.dump(output, args.logfile, width=2147483647, allow_unicode=True)
+
+    print(f"Wrote {len(items_found)} item locations and {len(loot_tables)} loot table locations to {outpath}")
+
+    if args.logfile is not sys.stdout:
+        args.logfile.close()
