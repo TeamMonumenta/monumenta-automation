cce275bb3465788ab854756acea349aeaf13e156
Timothy Southwick <NickNackGus@gmail.com>
2022-03-03 16:09:48 -0500

    Re-enabled the new chat plugin

--- utility_code/gen_server_config.py
+++ utility_code/gen_server_config.py
@@ -195,7 +195,7 @@ if __name__ == '__main__':
             ]
     else:
         purgatory_min += [
-                #('plugins/NetworkChat.jar', '../../server_config/plugins/NetworkChat.jar'),
+                ('plugins/NetworkChat.jar', '../../server_config/plugins/NetworkChat.jar'),
                 ('plugins/VentureChat.jar', '../../server_config/plugins/VentureChat.jar'),
                 ('plugins/VentureChat/config.yml', '../../../server_config/data/plugins/all/VentureChat/config.yml'),
             ]
