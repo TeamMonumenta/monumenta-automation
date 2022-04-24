593bc7c5b1c245612c0b6b0256a9c13c45d366a0
Byron Marohn <combustible@live.com>
2022-04-24 16:28:21 -0400

    Bump villager activation range to 25 on isles
    Signed-off-by: Byron Marohn <combustible@live.com>

--- utility_code/gen_server_config.py
+++ utility_code/gen_server_config.py
@@ -373,6 +373,7 @@ if __name__ == '__main__':
             'config':server_config_to_copy + [
                 ('server.properties', 'view-distance', 'view-distance=8'),
                 ('spigot.yml', 'view-distance', '    view-distance: 8'),
+                ('spigot.yml', '      villagers:', '      villagers: 25'),
             ],
             'linked':server_config + base_plugins + dynmap + [
                 ('plugins/Roguelite', '../../server_config/data/Roguelite'),
