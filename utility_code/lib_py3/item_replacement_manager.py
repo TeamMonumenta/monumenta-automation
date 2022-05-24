a397829aa53ea8aef66e68428d3cc8b70c092448
Luke Silvestri <silvestriluke@gmail.com>
2022-05-24 04:28:02 -0400

    removed tag from a few more places

--- utility_code/lib_py3/item_replacement_manager.py
+++ utility_code/lib_py3/item_replacement_manager.py
@@ -43,8 +43,8 @@ class ItemReplacementManager(object):
         if item.nbt.has_path('tag.display.Name'):
             # If a name isn't found, it can still be replaced with a named item.
             item_meta['name'] = get_item_name_from_nbt(item.tag)
-            if item.nbt.has_path('tag.Monumenta.Masterwork') and item.nbt.at_path('tag.Monumenta.Masterwork').value in ['1', '2', '3', '4', '5', '6', '7']:
-                item_meta['name'] = item_meta['name'] + '~m' + item.nbt.at_path('tag.Monumenta.Masterwork').value
+            if item.nbt.has_path('Monumenta.Masterwork') and item.nbt.at_path('Monumenta.Masterwork').value in ['1', '2', '3', '4', '5', '6', '7']:
+                item_meta['name'] = item_meta['name'] + '~m' + item.nbt.at_path('Monumenta.Masterwork').value
 
 
         # Substitute name/id values in case an item changed ID.
