2a6ea9eadde41b25d3aa4d91135fa1cd5d17ed68
Timothy Southwick <NickNackGus@gmail.com>
2022-05-26 16:37:03 -0400

    Removed season pass score handling, as this is done via plugin now

--- rust/src/bin/weekly_update_player_scores.rs
+++ rust/src/bin/weekly_update_player_scores.rs
@@ -80,10 +80,6 @@ fn update_player_scores(player: &mut Player, days_since_epoch: i32) {
         scores.insert("DFSAccess".to_string(), 0);
         scores.insert("DDAccess".to_string(), 0);
 
-        scores.insert("WeeklyMission1".to_string(), 0);
-        scores.insert("WeeklyMission2".to_string(), 0);
-        scores.insert("WeeklyMission3".to_string(), 0);
-
         fix_total_level(scores);
     }
 }
