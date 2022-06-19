2b22be4d96f0e883a5346232f597d2a4c4b4b7e5
Peter Güttinger <njol@gmx.ch>
2022-06-19 15:23:54 +0200

    Small performance improvement

--- utility_code/weekly_update_player_data.py
+++ utility_code/weekly_update_player_data.py
@@ -39,6 +39,7 @@ def process_player(player):
 
     return (num_replacements, replacements_log)
 
+substitute_items = SubtituteItems()
 def process_plugin_data(plugin_data):
     num_replacements = 0
     replacements_log = {}
@@ -49,9 +50,9 @@ def process_plugin_data(plugin_data):
     for item in plugin_data.vanity().iter_items():
         if item_replace_manager.replace_item(item, log_dict=replacements_log, debug_path=item.get_path_str()):
             num_replacements += 1
-    substitute_items = SubtituteItems()
     for cosmetic in plugin_data.cosmetics().iter_cosmetics():
         if cosmetic.type == 'vanity':
+            global substitute_items
             cosmetic.name = substitute_items.process_identifier(cosmetic.name)
 
     return (num_replacements, replacements_log)
