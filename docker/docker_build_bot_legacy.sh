940ffe235f2b5e6c65e701682e478dc7404e5ded
Byron Marohn <combustible@live.com>
2022-08-04 22:42:20 -0400

    Move play server automation bots to -legacy label for stability during update
    Signed-off-by: Byron Marohn <combustible@live.com>

---
+++ docker/docker_build_bot_legacy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pushd ../rust
+./build.sh || exit $?
+popd
+docker build .. --file automation-bot.Dockerfile -t docker.pkg.github.com/teammonumenta/monumenta-automation/monumenta-automation-bot-legacy --build-arg USERNAME=epic --build-arg UID=1000 --build-arg GID=1000 && docker push docker.pkg.github.com/teammonumenta/monumenta-automation/monumenta-automation-bot-legacy
