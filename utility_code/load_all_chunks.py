5d1466f58652691061aa75ca19cf35b897f79846
Byron Marohn <combustible@live.com>
2022-09-02 19:59:45 -0400

    Add tool to load all chunks in a region file for debugging
    Signed-off-by: Byron Marohn <combustible@live.com>

---
+++ utility_code/load_all_chunks.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env pypy3
+
+import os
+import sys
+import concurrent
+from minecraft.world import World
+from minecraft.region import Region, EntitiesRegion
+
+read_only = True
+
+def process_region(arg):
+    full_path, rx, rz, region_type = arg
+    region = region_type(full_path, rx, rz, read_only=read_only)
+
+    num_entities = 0
+    num_block_entities = 0
+    has_blocks = set()
+
+    for chunk in region.iter_chunks(autosave = not read_only):
+        # Do nothing - just loading chunks here
+        pass
+
+    print(f"Loaded all chunks for region {region}")
+    return True
+
+if __name__ == '__main__':
+    args = sys.argv
+    name = args.pop(0)
+    if len(sys.argv) == 0:
+        print("Usage: {} /path/to/world1 [/path/to/world2 ...]".format(name))
+        sys.exit()
+
+    regions = []
+    all_world_paths = []
+    for world_path in args:
+        if os.path.isfile(os.path.join(world_path, "level.dat")):
+            all_world_paths.append(world_path)
+        else:
+            # Not directly a world - maybe a folder containing worlds?
+            for enum_path in World.enumerate_worlds(world_path):
+                all_world_paths.append(os.path.join(world_path, enum_path))
+
+    if len(all_world_paths) <= 0:
+        sys.exit("No valid worlds found to load")
+
+    print(f"Loading all chunks from worlds:")
+    for world_path in all_world_paths:
+        print(f"  {world_path}")
+
+    for world_path in all_world_paths:
+        world = World(world_path)
+        for full_path, rx, rz, region_type in world.enumerate_regions():
+            regions.append((full_path, rx, rz, region_type))
+
+    for region_data in regions:
+        process_region(region_data)
+
+    print(f"Done")
