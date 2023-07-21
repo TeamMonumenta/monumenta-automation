2036c1e77d52b4f06da767d45084c88e3290d52e
Byron Marohn <combustible@live.com>
2023-07-20 22:11:34 -0400

    Link Monumenta/DataCollection -> 4_SHARED/DataCollection
    Signed-off-by: Byron Marohn <combustible@live.com>

--- utility_code/gen_server_config.py
+++ utility_code/gen_server_config.py
@@ -269,6 +269,7 @@ if __name__ == '__main__':
         ('plugins/Monumenta/experiencinator_config.json', '../../../server_config/data/plugins/all/Monumenta/experiencinator_config.json'),
         ('plugins/Monumenta/mmquest.json', '../../../server_config/data/plugins/all/Monumenta/mmquest.json'),
         ('plugins/Monumenta/ItemIndex', '../../../server_config/data/plugins/all/ItemIndex'),
+        ('plugins/Monumenta/DataCollection', '/home/epic/4_SHARED/DataCollection'),
         ('plugins/Monumenta/seasonalevents', '../../../server_config/data/plugins/all/Monumenta/seasonalevents'),
         ('plugins/Monumenta/InfinityTower/TowerMobs.json', '../../../../server_config/data/plugins/valley/Monumenta/InfinityTower/TowerMobs.json'),
         ('plugins/Monumenta/InfinityTower/InfinityTowerDefault.json', '../../../../server_config/data/plugins/valley/Monumenta/InfinityTower/InfinityTowerDefault.json'),
