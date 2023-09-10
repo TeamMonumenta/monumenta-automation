196ab5f263b8ea3e4a148e1d4493412be7e4bc9b
Byron Marohn <combustible@live.com>
2023-09-09 22:08:49 -0400

    Remove --illegal-access=permit, add
    Signed-off-by: Byron Marohn <combustible@live.com>

--- docker/monumenta.sh
+++ docker/monumenta.sh
@@ -14,7 +14,8 @@ check_var JAVA_JAR
 check_var JAVA_MEM
 check_var JAVA_META_MEM
 # Optional argument
-# JAVA_ARG
+# JAVA_ARG - Argument for Java
+# PAPER_ARG - Argument for Paper
 
 if [[ ! -d "$SERVER_DIR" ]]; then
 	echo "SERVER_DIR='$SERVER_DIR' does not exist!"
@@ -29,9 +30,9 @@ if [[ "$JAVA_LARGE_PAGES " != " " ]]; then
 fi
 
 # Remaining standard arguments
-args="$args -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -Dlog4j2.formatMsgNoLookups=true --illegal-access=permit"
+args="$args -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -Dlog4j2.formatMsgNoLookups=true"
 
 cd "$SERVER_DIR"
 echo "Executing:"
-echo java $args -jar "$JAVA_JAR" $JAVA_ARG
-exec java $args -jar "$JAVA_JAR" $JAVA_ARG
+echo java $args $JAVA_ARG -jar "$JAVA_JAR" $PAPER_ARG
+exec java $args $JAVA_ARG -jar "$JAVA_JAR" $PAPER_ARG
