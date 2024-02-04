d699a84980c494547dcc7cadb00516ad63e13a06
Byron Marohn <combustible@live.com>
2024-02-04 03:42:16 -0500

    Build/stage/volt migration changes
    Signed-off-by: Byron Marohn <combustible@live.com>

--- docker/docker_build_bot_volt.sh
+++ docker/docker_build_bot_volt.sh
@@ -3,4 +3,4 @@
 pushd ../rust
 ./build.sh || exit $?
 popd
-docker build .. --file automation-bot.Dockerfile -t docker.pkg.github.com/teammonumenta/monumenta-automation/monumenta-automation-bot-volt --build-arg USERNAME=epic --build-arg UID=1000 --build-arg GID=1000 && docker push docker.pkg.github.com/teammonumenta/monumenta-automation/monumenta-automation-bot-volt
+docker build .. --file automation-bot.Dockerfile -t ghcr.io/teammonumenta/monumenta-automation/monumenta-automation-bot-volt --build-arg USERNAME=epic --build-arg UID=1000 --build-arg GID=1000 && docker push ghcr.io/teammonumenta/monumenta-automation/monumenta-automation-bot-volt
