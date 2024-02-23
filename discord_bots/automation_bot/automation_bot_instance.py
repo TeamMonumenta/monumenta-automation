1d55dc19c532e96e2504acc123ebf4ac63731191
Timothy Southwick <NickNackGus@gmail.com>
2024-02-22 19:35:45 -0500

    Minimize redis data on ~stage

--- discord_bots/automation_bot/automation_bot_instance.py
+++ discord_bots/automation_bot/automation_bot_instance.py
@@ -2149,6 +2149,7 @@ Archives the previous stage server contents under 0_PREVIOUS '''
         await self.display(ctx, "Downloading current redis database from the play server...")
         await self.run(ctx, f"redis-cli -h redis.play --rdb dump.rdb")
         await self.start(ctx, "redis", owner=message)
+        await self.run(ctx, [os.path.join(_top_level, f"rust/bin/redis_truncate_playerdata"), 'redis://redis/', 'play', '1'], displayOutput=True)
 
         # Download the mysql database from the play server
         await self.display(ctx, "Syncing with current mysql database from the play server...")
