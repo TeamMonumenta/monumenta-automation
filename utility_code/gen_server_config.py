40c478a7c6ddf2a03fa06ec0d5f4e1d073daf269
Byron Marohn <combustible@live.com>
2024-03-12 17:21:48 -0400

    Link Monumenta/market.json
    Signed-off-by: Byron Marohn <combustible@live.com>

--- utility_code/gen_server_config.py
+++ utility_code/gen_server_config.py
@@ -272,6 +272,7 @@ if __name__ == '__main__':
         ('plugins/Monumenta/experiencinator_config.json', '../../../server_config/data/plugins/all/Monumenta/experiencinator_config.json'),
         ('plugins/Monumenta/SiriusBlightArena.json', '../../../server_config/data/plugins/all/Monumenta/SiriusBlightArena.json'),
         ('plugins/Monumenta/mmquest.json', '../../../server_config/data/plugins/all/Monumenta/mmquest.json'),
+        ('plugins/Monumenta/market.json', '../../../server_config/data/plugins/all/Monumenta/market.json'),
         ('plugins/Monumenta/ItemIndex', '../../../server_config/data/plugins/all/ItemIndex'),
         ('plugins/Monumenta/DataCollection', '/home/epic/4_SHARED/DataCollection'),
         ('plugins/Monumenta/DepthsStats', '/home/epic/4_SHARED/DepthsStats'),
