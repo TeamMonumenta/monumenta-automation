96c623ec2560aed965dbbc1a46a56432258bf7ae
Timothy Southwick <NickNackGus@gmail.com>
2024-09-12 21:58:10 -0400

    REVERT ME: Temporarily reset Guild scores for all players

--- rust/src/bin/weekly_update_player_scores.rs
+++ rust/src/bin/weekly_update_player_scores.rs
@@ -90,6 +90,8 @@ fn update_player_scores(player: &mut Player, days_since_epoch: i32) {
         scores.insert("GodsporeAccess".to_string(), 0);
         scores.insert("AzacorAccess".to_string(), 0);
         scores.insert("DCZAccess".to_string(), 0);
+        // Temporarily reset Guild score for use on guildplots (avoid sending people to other peoples' guilds by mistake)
+        scores.insert("Guild".to_string(), 0);
 
         fix_total_level(scores);
     }
