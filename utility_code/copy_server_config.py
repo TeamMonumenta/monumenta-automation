ad8c08f0c658fed155a55fe164a5cc1e314628bd
violet <v.violette.yy@gmail.com>
2025-05-31 00:00:47 +0800

    copy server_config_template

--- utility_code/copy_server_config.py
+++ utility_code/copy_server_config.py
@@ -10,11 +10,23 @@ def main():
         if "-" not in jar.name:
             shutil.copy(jar, dst)
             print(f"Copied {jar} to {dst}")
+
+    os.makedirs(os.path.join(dst, 'plugins'), exist_ok=True)
     
     for jar in list(Path(os.path.join(src, 'plugins')).glob("*.jar")):
         if "-" not in jar.name:
             shutil.copy(jar, os.path.join(dst, 'plugins'))
             print(f"Copied {jar} to {os.path.join(dst, 'plugins')}")
+
+    data_include = [
+        "server_config_template"
+    ]
+
+    for item in data_include:
+        shutil.copy(
+            os.path.join(src, "data", item),
+            os.path.join(dst, "data", item)
+        )
     
 if __name__ == "__main__":
     main()
