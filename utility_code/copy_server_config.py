96682e358d4cc27c368bd517370b09a98e523797
violet <v.violette.yy@gmail.com>
2025-05-30 23:43:08 +0800

    copy jars

---
+++ utility_code/copy_server_config.py
@@ -0,0 +1,20 @@
+import shutil
+import os
+from pathlib import Path
+
+def main():
+    src = os.path.expanduser("~/project_epic/server_config")
+    dst = os.path.expanduser(os.getcwd(), "server_config")
+
+    for jar in list(Path(src).glob("*.jar")):
+        if "-" not in jar.name:
+            shutil.copy(jar, dst)
+        print(f"Copied {jar} to {dst}")
+    
+    for jar in list(Path(os.path.join(src, 'plugins')).glob("*.jar")):
+        if "-" not in jar.name:
+            shutil.copy(jar, os.path.join(dst, 'plugins'))
+        print(f"Copied {jar} to {os.path.join(dst, 'plugins')}")
+    
+if __name__ == "__main__":
+    main()
