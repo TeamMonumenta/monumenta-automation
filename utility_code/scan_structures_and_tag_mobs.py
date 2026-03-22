903723b1837e7e1c958d01ba98f9d17f99f7aa69
Timothy Southwick <NickNackGus@gmail.com>
2026-03-22 16:15:22 -0400

    Added fortune shard

--- utility_code/scan_structures_and_tag_mobs.py
+++ utility_code/scan_structures_and_tag_mobs.py
@@ -171,6 +171,7 @@ if __name__ == '__main__':
         "ruin":{"path": "ring/ruin"},
         "skt":{"world": "SKT"},
         "indigo":{"world": "indigo"},
+        "fortune":{"world": "fortune"},
         "hexfall":{"world": "hexfall"},
     }
 
